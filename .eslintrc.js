1efa2fa9c6c809672d853023f05fbb8bde623a4e
nklomp <nklomp@sphereon.com>
2021-11-28 19:08:55 +0100

    chore: eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 13,
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+  },
+};
