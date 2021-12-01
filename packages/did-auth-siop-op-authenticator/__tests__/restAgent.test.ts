baf3bd8fd66779857cde2b1f7a6a1fca829ead9e
Bram ten Cate <btencate@sphereon.com>
2021-12-01 11:51:40 +0100

    MYC-106 added did auth siop tests

---
+++ packages/did-auth-siop-op-authenticator/__tests__/restAgent.test.ts
@@ -0,0 +1,72 @@
+import 'cross-fetch/polyfill'
+import express from 'express'
+import {
+  IAgent,
+  createAgent,
+  IAgentOptions,
+} from '@veramo/core'
+import { AgentRestClient } from '@veramo/remote-client'
+import { Server } from 'http'
+import { AgentRouter, RequestWithAgentRouter } from '@veramo/remote-server'
+import { getConfig } from '@veramo/cli/build/setup'
+import { createObjects } from '@veramo/cli/build/lib/objectCreator'
+import { DidAuthSiopOpAuthenticator } from '../src/agent/DidAuthSiopOpAuthenticator';
+import { IDidAuthSiopOpAuthenticator } from '../src/types/IDidAuthSiopOpAuthenticator'
+import didAuthSiopOpAuthenticatorAgentLogic from './shared/didAuthSiopOpAuthenticatorAgentLogic'
+
+jest.setTimeout(30000);
+
+const port = 3002;
+const basePath = '/agent';
+let serverAgent: IAgent;
+let restServer: Server;
+
+const getAgent = (options?: IAgentOptions) =>
+  createAgent<IDidAuthSiopOpAuthenticator>({
+    ...options,
+    plugins: [
+      new DidAuthSiopOpAuthenticator({
+        did: 'did:ethr:0xcBe71d18b5F1259faA9fEE8f9a5FAbe2372BE8c9',
+        kid: 'did:ethr:0xcBe71d18b5F1259faA9fEE8f9a5FAbe2372BE8c9',
+        privateKey: 'ea6aaeebe1755...',
+      }),
+      new AgentRestClient({
+        url: 'http://localhost:' + port + basePath,
+        enabledMethods: serverAgent.availableMethods(),
+        schema: serverAgent.getSchema(),
+      }),
+    ],
+  });
+
+const setup = async (): Promise<boolean> => {
+  const config = getConfig('packages/did-auth-siop-op-authenticator/agent.yml');
+  const { agent } = createObjects(config, { agent: '/agent'});
+  serverAgent = agent;
+
+  const agentRouter = AgentRouter({
+    exposedMethods: serverAgent.availableMethods(),
+  });
+
+  const requestWithAgent = RequestWithAgentRouter({
+    agent: serverAgent,
+  });
+
+  return new Promise((resolve) => {
+    const app = express();
+    app.use(basePath, requestWithAgent, agentRouter);
+    restServer = app.listen(port, () => {
+      resolve(true)
+    })
+  })
+};
+
+const tearDown = async (): Promise<boolean> => {
+  restServer.close();
+  return true
+};
+
+const testContext = { getAgent, setup, tearDown };
+
+describe('REST integration tests', () => {
+  didAuthSiopOpAuthenticatorAgentLogic(testContext)
+});
---
+++ packages/did-auth-siop-op-authenticator/__tests__/shared/didAuthSiopOpAuthenticatorAgentLogic.ts
@@ -0,0 +1,197 @@
+import { TAgent } from '@veramo/core'
+import { OP, PresentationExchange } from '@sphereon/did-auth-siop/dist/main'
+import { IDidAuthSiopOpAuthenticator } from '../../src/types/IDidAuthSiopOpAuthenticator'
+import {
+  ResponseContext,
+  ResponseMode,
+  ResponseType,
+  SubjectIdentifierType,
+  UrlEncodingFormat,
+  VerificationMode,
+} from '@sphereon/did-auth-siop/dist/main/types/SIOP.types';
+
+const nock = require('nock');
+
+type ConfiguredAgent = TAgent<IDidAuthSiopOpAuthenticator>
+
+const didMethod = 'ethr';
+const did = `did:${didMethod}:0xcBe71d18b5F1259faA9fEE8f9a5FAbe2372BE8c9`;
+const redirectUrl = 'http://example/ext/get-auth-request-url';
+const stateId = '2hAyTM7PB3SGJaeGU7QeTJ';
+const nonce = 'o5qwML7DnrcLMs9Vdizyz9';
+const scope = 'openid';
+const requestResultMockedText =
+    'openid://?response_type=id_token' +
+    '&scope=openid' +
+    '&client_id=' + did +
+    '&redirect_uri=' + redirectUrl +
+    '&iss=' + did +
+    '&response_mode=post' +
+    '&response_context=rp' +
+    '&nonce=' + nonce +
+    '&state=' + stateId +
+    '&registration=registration_value' +
+    '&request=ey...';
+const registration = {
+  did_methods_supported: [`did:${didMethod}:`],
+  subject_identifiers_supported: SubjectIdentifierType.DID,
+  credential_formats_supported: []
+}
+const authenticationRequest = {
+  encodedUri: 'uri_example',
+  encodingFormat: UrlEncodingFormat.FORM_URL_ENCODED,
+  jwt: 'ey...',
+  requestPayload:{
+    response_type: ResponseType.ID_TOKEN,
+    scope,
+    client_id: did,
+    redirect_uri: redirectUrl,
+    iss: did,
+    response_mode: ResponseMode.POST,
+    response_context: ResponseContext.RP,
+    nonce,
+    stateId,
+    registration,
+    request: 'ey...'
+  },
+  registration
+}
+const authenticationVerificationMockedResult = {
+  payload: {},
+  verifyOpts: {}
+}
+const createAuthenticationResponseMockedResult = {
+  didResolutionResult: {
+    didResolutionMetadata: {},
+    didDocument: {
+      id: did,
+    },
+    didDocumentMetadata: {}
+  },
+  issuer: did,
+  signer: {
+    id: did,
+    type: 'authentication',
+    controller: did,
+  },
+  jwt: 'ey...',
+  payload: {
+    scope,
+    response_type: ResponseType.ID_TOKEN,
+    client_id: did,
+    redirect_uri: redirectUrl,
+    response_mode: ResponseMode.POST,
+    response_context: ResponseContext.RP,
+    nonce: nonce,
+  },
+  verifyOpts: {
+    verification: {
+      mode: VerificationMode.INTERNAL,
+      resolveOpts: {}
+    }
+  }
+}
+
+export default (testContext: {
+  getAgent: () => ConfiguredAgent
+  setup: () => Promise<boolean>
+  tearDown: () => Promise<boolean>
+}) => {
+  describe('DID Auth SIOP OP Authenticator Agent Plugin', () => {
+    let agent: ConfiguredAgent;
+
+    beforeAll(() => {
+      testContext.setup();
+      agent = testContext.getAgent()
+
+      nock(redirectUrl)
+        .get(`?stateId=${stateId}`)
+        .times(4)
+        .reply(200, requestResultMockedText)
+
+      const mockedParseAuthenticationRequestURIMethod = jest.fn()
+      OP.prototype.parseAuthenticationRequestURI = mockedParseAuthenticationRequestURIMethod
+      mockedParseAuthenticationRequestURIMethod.mockReturnValue(Promise.resolve(authenticationRequest))
+
+      const mockedVerifyAuthenticationRequestMethod = jest.fn()
+      OP.prototype.verifyAuthenticationRequest = mockedVerifyAuthenticationRequestMethod
+      mockedVerifyAuthenticationRequestMethod.mockReturnValue(Promise.resolve(authenticationVerificationMockedResult))
+
+      const mockedCreateAuthenticationResponse = jest.fn()
+      OP.prototype.createAuthenticationResponse = mockedCreateAuthenticationResponse
+      mockedCreateAuthenticationResponse.mockReturnValue(Promise.resolve(createAuthenticationResponseMockedResult))
+
+      const mockSubmitAuthenticationResponseMethod = jest.fn()
+      OP.prototype.submitAuthenticationResponse = mockSubmitAuthenticationResponseMethod
+      mockSubmitAuthenticationResponseMethod.mockReturnValue(Promise.resolve({status: 200, statusText: 'example_value' }))
+
+      const mockedSelectVerifiableCredentialsForSubmissionMethod = jest.fn()
+      PresentationExchange.prototype.selectVerifiableCredentialsForSubmission = mockedSelectVerifiableCredentialsForSubmissionMethod
+      mockedSelectVerifiableCredentialsForSubmissionMethod.mockReturnValue(Promise.resolve({errors: [], matches: ['match']}))
+
+      const mockedSubmissionFromMethod = jest.fn()
+      PresentationExchange.prototype.submissionFrom = mockedSubmissionFromMethod
+      mockedSubmissionFromMethod.mockReturnValue(Promise.resolve({}))
+    });
+
+    afterAll(testContext.tearDown);
+
+    it('should authentication with DID SIOP', async () => {
+      const result = await agent.authenticateWithDidSiop({
+        stateId,
+        redirectUrl,
+        didMethod,
+      });
+
+      expect(result.status).toEqual(200)
+    });
+
+    it('should get authentication request from RP', async () => {
+      const result = await agent.getDidSiopAuthenticationRequestFromRP({
+        stateId,
+        redirectUrl,
+      });
+
+      expect(result).toEqual(authenticationRequest)
+    });
+
+    it('should get authentication details', async () => {
+      const result = await agent.getDidSiopAuthenticationRequestDetails({
+        verifiedAuthenticationRequest: createAuthenticationResponseMockedResult,
+        verifiableCredentials: []
+      });
+
+      expect(result.id).toEqual(did)
+    });
+
+    it('should verify authentication request URI with did methods supported provided', async () => {
+      authenticationRequest.registration.did_methods_supported = [`did:${didMethod}:`];
+
+      const result = await agent.verifyDidSiopAuthenticationRequestURI({
+        requestURI: authenticationRequest
+      });
+
+      expect(result).toEqual(authenticationVerificationMockedResult)
+    });
+
+    it('should verify authentication request URI without did methods supported provided', async () => {
+      authenticationRequest.registration.did_methods_supported = [];
+
+      const result = await agent.verifyDidSiopAuthenticationRequestURI({
+        requestURI: authenticationRequest,
+        didMethod
+      });
+
+      expect(result).toEqual(authenticationVerificationMockedResult)
+    });
+
+    it('should send authentication response', async () => {
+      const result = await agent.sendDidSiopAuthenticationResponse({
+        verifiedAuthenticationRequest: createAuthenticationResponseMockedResult
+      });
+
+      expect(result.status).toEqual(200)
+    });
+
+  })
+};
