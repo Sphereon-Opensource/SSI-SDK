abdace14f9441e667e5e9703b6d9f561ede090c6
hrehman-sphereon <hrehman@sphereon.com>
2021-12-07 11:21:10 +0100

    MYC-140 fix prettier errors

--- packages/vc-api-verifier/__tests__/localAgent.test.ts
+++ packages/vc-api-verifier/__tests__/localAgent.test.ts
@@ -1,27 +1,27 @@
-import { getConfig } from '@veramo/cli/build/setup';
-import { createObjects } from '@veramo/cli/build/lib/objectCreator';
+import { getConfig } from '@veramo/cli/build/setup'
+import { createObjects } from '@veramo/cli/build/lib/objectCreator'
 
-jest.setTimeout(30000);
+jest.setTimeout(30000)
 
-import vcApiVerifierAgentLogic from './shared/vcApiVerifierAgentLogic';
+import vcApiVerifierAgentLogic from './shared/vcApiVerifierAgentLogic'
 
-let agent: any;
+let agent: any
 
 const setup = async (): Promise<boolean> => {
-  const config = getConfig('packages/vc-api-verifier/agent.yml');
-  const { localAgent } = createObjects(config, { localAgent: '/agent' });
-  agent = localAgent;
+  const config = getConfig('packages/vc-api-verifier/agent.yml')
+  const { localAgent } = createObjects(config, { localAgent: '/agent' })
+  agent = localAgent
 
-  return true;
-};
+  return true
+}
 
 const tearDown = async (): Promise<boolean> => {
-  return true;
-};
+  return true
+}
 
-const getAgent = () => agent;
-const testContext = { getAgent, setup, tearDown };
+const getAgent = () => agent
+const testContext = { getAgent, setup, tearDown }
 
 describe('Local integration tests', () => {
-  vcApiVerifierAgentLogic(testContext);
-});
+  vcApiVerifierAgentLogic(testContext)
+})
--- packages/vc-api-verifier/__tests__/restAgent.test.ts
+++ packages/vc-api-verifier/__tests__/restAgent.test.ts
@@ -1,22 +1,21 @@
-import 'cross-fetch/polyfill';
-import { IAgent, createAgent, IAgentOptions } from '@veramo/core';
-import { AgentRestClient } from '@veramo/remote-client';
-import express from 'express';
-import { Server } from 'http';
-import { AgentRouter, RequestWithAgentRouter } from '@veramo/remote-server';
-import { getConfig } from '@veramo/cli/build/setup';
-import { createObjects } from '@veramo/cli/build/lib/objectCreator';
-import { IVcApiVerifier } from '../src/types/IVcApiVerifier';
-import { VcApiVerifier } from '../src/agent/VcApiVerifier';
-import vcApiVerifierAgentLogic from './shared/vcApiVerifierAgentLogic';
-import * as path from "path";
+import 'cross-fetch/polyfill'
+import { IAgent, createAgent, IAgentOptions } from '@veramo/core'
+import { AgentRestClient } from '@veramo/remote-client'
+import express from 'express'
+import { Server } from 'http'
+import { AgentRouter, RequestWithAgentRouter } from '@veramo/remote-server'
+import { getConfig } from '@veramo/cli/build/setup'
+import { createObjects } from '@veramo/cli/build/lib/objectCreator'
+import { IVcApiVerifier, VcApiVerifier } from '../src'
+import vcApiVerifierAgentLogic from './shared/vcApiVerifierAgentLogic'
+import * as path from 'path'
 
-jest.setTimeout(30000);
+jest.setTimeout(30000)
 
-const port = 3003;
-const basePath = '/agent';
-let serverAgent: IAgent;
-let restServer: Server;
+const port = 3003
+const basePath = '/agent'
+let serverAgent: IAgent
+let restServer: Server
 
 const getAgent = (options?: IAgentOptions) =>
   createAgent<IVcApiVerifier>({
@@ -31,37 +30,37 @@ const getAgent = (options?: IAgentOptions) =>
         schema: serverAgent.getSchema(),
       }),
     ],
-  });
+  })
 
 const setup = async (): Promise<boolean> => {
-  const config = getConfig(path.resolve('packages/vc-api-verifier/agent.yml'));
-  const { agent } = createObjects(config, { agent: '/agent' });
-  serverAgent = agent;
+  const config = getConfig(path.resolve('packages/vc-api-verifier/agent.yml'))
+  const { agent } = createObjects(config, { agent: '/agent' })
+  serverAgent = agent
 
   const agentRouter = AgentRouter({
     exposedMethods: serverAgent.availableMethods(),
-  });
+  })
 
   const requestWithAgent = RequestWithAgentRouter({
     agent: serverAgent,
-  });
+  })
 
   return new Promise((resolve) => {
-    const app = express();
-    app.use(basePath, requestWithAgent, agentRouter);
+    const app = express()
+    app.use(basePath, requestWithAgent, agentRouter)
     restServer = app.listen(port, () => {
-      resolve(true);
-    });
-  });
-};
+      resolve(true)
+    })
+  })
+}
 
 const tearDown = async (): Promise<boolean> => {
-  restServer.close();
-  return true;
-};
+  restServer.close()
+  return true
+}
 
-const testContext = { getAgent, setup, tearDown };
+const testContext = { getAgent, setup, tearDown }
 
 describe('REST integration tests', () => {
-  vcApiVerifierAgentLogic(testContext);
-});
+  vcApiVerifierAgentLogic(testContext)
+})
