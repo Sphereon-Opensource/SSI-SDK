5c18dc2a2f61ddff213595408ad10b62a5e83476
nklomp <nklomp@sphereon.com>
2021-12-10 02:36:29 +0100

    feat: Add suite lookup based on verification method type next to veramo key type

--- packages/vc-handler-ld-local/src/ld-suite-loader.ts
+++ packages/vc-handler-ld-local/src/ld-suite-loader.ts
@@ -1,25 +1,28 @@
-import { VeramoLdSignature } from '@veramo/credential-ld'
 import { TKeyType } from '@veramo/core'
+import { SphereonLdSignature } from './ld-suites'
 
 /**
  * Initializes a list of Veramo-wrapped LD Signature suites and exposes those to the Agent Module
  */
 export class LdSuiteLoader {
-  constructor(options: { veramoLdSignatures: VeramoLdSignature[] }) {
-    options.veramoLdSignatures.forEach((obj) => {
+  constructor(options: { ldSignatureSuites: SphereonLdSignature[] }) {
+    options.ldSignatureSuites.forEach((obj) => {
       this.signatureMap[obj.getSupportedVeramoKeyType()] = obj
+      this.signatureMap[obj.getSupportedVerificationType()] = obj
     })
   }
-  private signatureMap: Record<string, VeramoLdSignature> = {}
+  private signatureMap: Record<string, SphereonLdSignature> = {}
 
-  getSignatureSuiteForKeyType(type: TKeyType): VeramoLdSignature {
-    const suite = this.signatureMap[type]
+  getSignatureSuiteForKeyType(type: TKeyType, verificationType?: string): SphereonLdSignature {
+    // Always use verification type if supplied. This is the type denoted by the DID verification method type
+
+    const suite = verificationType && this.signatureMap[verificationType] ? this.signatureMap[verificationType] : this.signatureMap[type]
     if (suite) return suite
 
-    throw new Error('No Veramo LD Signature Suite for ' + type)
+    throw new Error('No Sphereon or Veramo LD Signature Suite for ' + type)
   }
 
-  getAllSignatureSuites(): VeramoLdSignature[] {
+  getAllSignatureSuites(): SphereonLdSignature[] {
     return Object.values(this.signatureMap)
   }
 
