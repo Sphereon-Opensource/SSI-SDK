4a3b8ce93e36c55b3b17884e262db9d91c4459e6
nklomp <nklomp@sphereon.com>
2022-01-16 18:18:08 +0100

    fix: Add missing suites exports

--- packages/vc-handler-ld-local/src/index.ts
+++ packages/vc-handler-ld-local/src/index.ts
@@ -3,5 +3,6 @@
  */
 const schema = require('../plugin.schema.json')
 export { schema }
+export { SphereonEd25519Signature2018, SphereonEd25519Signature2020, SphereonEcdsaSecp256k1RecoverySignature2020 } from './suites'
 export { CredentialHandlerLDLocal } from './agent/CredentialHandlerLDLocal'
 export * from './types/ICredentialHandlerLDLocal'
