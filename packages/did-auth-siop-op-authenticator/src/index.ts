70bba4ee4ead021ebce6f1a1f1f605bf8daed77f
Bram ten Cate <btencate@sphereon.com>
2022-01-18 17:41:31 +0100

    MYC-146 did-auth-siop-op-authenticator added export for OpSession

--- packages/did-auth-siop-op-authenticator/src/index.ts
+++ packages/did-auth-siop-op-authenticator/src/index.ts
@@ -4,4 +4,5 @@
 const schema = require('../plugin.schema.json')
 export { schema }
 export { DidAuthSiopOpAuthenticator } from './agent/DidAuthSiopOpAuthenticator'
+export { OpSession } from './session/OpSession'
 export * from './types/IDidAuthSiopOpAuthenticator'
