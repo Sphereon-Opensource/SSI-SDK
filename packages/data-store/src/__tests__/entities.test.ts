431f06e2fadea165b6636fcd6960dabd71716218
A.G.J. Cate <brummos@gmail.com>
2022-05-25 11:35:30 +0200

    MYC-174 data-store use in memory db for tests

--- packages/data-store/src/__tests__/entities.test.ts
+++ packages/data-store/src/__tests__/entities.test.ts
@@ -18,7 +18,7 @@ import {
 
 describe('Database entities test', () => {
   let dbConnection: Connection
-  const databaseFile = './packages/data-store/database.sqlite'
+  // const databaseFile = './packages/data-store/database.sqlite'
   const connection_relations = [
     'config',
     'metadata',
@@ -29,7 +29,7 @@ describe('Database entities test', () => {
     async () =>
       (dbConnection = await createConnection({
         type: 'sqlite',
-        database: databaseFile,
+        database: ':memory:',//databaseFile,
         entities: Entities
       }))
   )
@@ -41,7 +41,7 @@ describe('Database entities test', () => {
 
   afterAll(async () => {
     await dbConnection.close()
-    fs.unlinkSync(databaseFile)
+    // fs.unlinkSync(databaseFile)
   })
 
   it('Should save party to database', async () => {
