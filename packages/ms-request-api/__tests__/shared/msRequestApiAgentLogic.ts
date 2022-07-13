e6a39f68d88b2518813047f3a8cfdb50e102e531
mehmetbicak <mehmetbicak@gmail.com>
2022-07-13 09:53:33 +0200

    VDX-68 Save and Fetch VC Test code

--- packages/ms-request-api/__tests__/shared/msRequestApiAgentLogic.ts
+++ packages/ms-request-api/__tests__/shared/msRequestApiAgentLogic.ts
@@ -1,10 +1,14 @@
 import * as MsAuthenticator from '@sphereon/ms-authenticator'
 import { fetchIssuanceRequestMs } from '../../src/IssuerUtil'
 import { IMsRequestApi, IIssueRequestResponse, IClientIssueRequest, IClientIssuanceConfig } from '../../src/types/IMsRequestApi'
-import { TAgent } from '@veramo/core'
 import { v4 as uuidv4 } from 'uuid'
+import { createAgent, TAgent , IDataStore, IDataStoreORM, VerifiableCredential, FindArgs, TCredentialColumns} from '@veramo/core'
+import { Entities, DataStore, DataStoreORM } from '@veramo/data-store'
+import { createConnection } from 'typeorm'
 
-type ConfiguredAgent = TAgent<IMsRequestApi>
+type ConfiguredAgent = TAgent<IMsRequestApi & IDataStore & IDataStoreORM >;
+const did1 = 'did:test:111';
+const did2 = 'did:test:222';
 var requestIssuanceResponse: IIssueRequestResponse = {
   requestId: '2e5c6fae-218c-4c7b-8440-df5454f908e9',
   url: 'www.google.com',
@@ -73,6 +77,59 @@ export default (testContext: { getAgent: () => ConfiguredAgent; setup: () => Pro
       fetchIssuanceRequestMs.prototype = fetchIssuanceRequestMsMock
 
       return await expect(agent.issuanceRequestMsVc(clientIssueRequest)).resolves.not.toBeNull
+    })
+
+    it('should store credential and retrieve by id', async () => {
+
+      const dbConnection = createConnection({
+        type: 'sqlite',
+        database: ':memory:',
+        synchronize: true,
+        logging: false,
+        entities: Entities,
+      })
+      const localAgent = createAgent<IDataStore & IDataStoreORM >({
+        plugins: [
+          new DataStore(dbConnection),
+          new DataStoreORM(dbConnection)
+        ],
+      })
+
+      const vc5: VerifiableCredential = {
+        '@context': ['https://www.w3.org/2018/credentials/v1323', 'https://www.w3.org/2020/demo/4342323'],
+        type: ['VerifiableCredential', 'PublicProfile'],
+        issuer: { id: did1 },
+        issuanceDate: new Date().toISOString(),
+        id: 'vc5',
+        credentialSubject: {
+          id: did2,
+          name: 'Alice',
+          profilePicture: 'https://example.com/a.png',
+          address: {
+            street: 'Some str.',
+            house: 1,
+          },
+        },
+        proof: {
+          jwt: 'mockJWT',
+        },
+      }
+
+      await localAgent.dataStoreSaveVerifiableCredential({ verifiableCredential: vc5 })
+
+      const args: FindArgs<TCredentialColumns> = {
+        where: [
+          {
+            column: 'id',
+            value: ['vc5'],
+          },
+        ]
+      }
+
+      const credentials = await localAgent.dataStoreORMGetVerifiableCredentialsByClaims({})
+      expect(credentials[0].verifiableCredential.id).toEqual('vc5')
+      const count = await localAgent.dataStoreORMGetVerifiableCredentialsCount(args)
+      expect(count).toEqual(1)
     })
   })
 }
