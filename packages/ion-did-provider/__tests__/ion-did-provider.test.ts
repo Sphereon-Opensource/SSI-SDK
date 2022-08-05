384cf6edfc7ead017136f3f6cc2d2b9c2582ede3
nklomp <nklomp@sphereon.com>
2022-08-05 23:31:38 +0200

    chore: Ensure DID creation test works

--- packages/ion-did-provider/__tests__/ion-did-provider.test.ts
+++ packages/ion-did-provider/__tests__/ion-did-provider.test.ts
@@ -1,76 +1,63 @@
-import { IKeyManager, IKeyManagerCreateArgs, ManagedKeyInfo, MinimalImportableKey } from '@veramo/core'
+import { createAgent, IKeyManager } from '@veramo/core'
 import { DIDManager, MemoryDIDStore } from '@veramo/did-manager'
+import { KeyManager, MemoryKeyStore, MemoryPrivateKeyStore } from '@veramo/key-manager'
+import { KeyManagementSystem } from '@veramo/kms-local'
 import { IonDIDProvider } from '../src'
-import { IRequiredContext } from '../src/types/ion-provider-types'
+import { VerificationRelationship } from '../src/types/ion-provider-types'
 
-/*
-const PRIVATE_KEY_HEX =
-  'ea6aaeebe17557e0fe256bfce08e8224a412ea1e25a5ec8b5d69618a58bad89e89a4661e446b46401325a38d3b20582d1dd277eb448a3181012a671b7ae15837'
-const PUBLIC_KEY_HEX = '89a4661e446b46401325a38d3b20582d1dd277eb448a3181012a671b7ae15837'
-*/
-
-// const kms = new KeyManagementSystem(new MemoryPrivateKeyStore())
-const memoryDIDStore = new MemoryDIDStore()
 
 const ionDIDProvider = new IonDIDProvider({
-  defaultKms: 'local',
+  defaultKms: 'mem',
+})
+const agent = createAgent<IKeyManager, DIDManager>({
+  plugins: [
+    new KeyManager({
+      store: new MemoryKeyStore(),
+      kms: {
+        mem: new KeyManagementSystem(new MemoryPrivateKeyStore()),
+      },
+    }),
+    new DIDManager({
+      providers: {
+        'did:ion': ionDIDProvider,
+      },
+      defaultProvider: 'did:ion',
+      store: new MemoryDIDStore(),
+    }),
+  ],
 })
 
-const didManager = new DIDManager({
-  providers: { 'did:ion': ionDIDProvider },
-  defaultProvider: 'did:ion',
-  store: memoryDIDStore,
-})
 
 describe('@sphereon/ion-did-provider', () => {
-  const mockContext = {
-    agent: {
-      keyManagerCreate(_args: IKeyManagerCreateArgs): Promise<ManagedKeyInfo> {
-        return Promise.resolve({ publicKeyHex: 'aabbcc', kid: 'testKid2' } as ManagedKeyInfo)
-      },
-      keyManagerImport(args: MinimalImportableKey): Promise<ManagedKeyInfo> {
-        return Promise.resolve({
-          publicKeyHex: args.publicKeyHex || 'aabbcc',
-          type: args.type,
-          kms: args.kms,
-          kid: args.kid || 'testKid2',
-        })
-      },
-    } as IKeyManager,
-  } as IRequiredContext
-
   it('should create identifier', async () => {
     jest.setTimeout(100000)
 
-    // jest.spyOn(fetch, '').mockResolvedValueOnce(Promise.resolve(restResponse));
-    const identifier = await ionDIDProvider.createIdentifier(
-      {
-        options: {},
+    const identifier = await agent.didManagerCreate({
+        options: {
+          anchor: false,
+          recoveryKey: {
+            kid: 'recovery-test'
+          },
+          updateKey: {
+            kid: 'update-test'
+          },
+          verificationMethods: [
+            {
+              kid: 'did1-test',
+              purposes: [ VerificationRelationship.authentication, VerificationRelationship.assertionMethod]
+            },
+            {
+              kid: 'did2-test',
+              purposes: [ VerificationRelationship.keyAgreement]
+            }
+          ]
+        },
       },
-      mockContext
     )
 
     expect(identifier).toBeDefined()
-    if (didManager == null) {
-      console.log("nothing")
-    }
-    // console.log(await didManager.didManagerGet(identifier))
-    // expect.assertions(4)
-    // return assertExpectedIdentifier(identifier)
+    console.log(JSON.stringify(identifier.did, null, 2))
+
+    console.log(JSON.stringify(await agent.didManagerGet(identifier)), null, 2)
   })
 })
-/*
-
-async function assertExpectedIdentifier(identifier: Promise<Omit<IIdentifier, 'provider'>>) {
-  await expect(identifier).resolves.toHaveProperty('did', LTO_DID)
-  await expect(identifier).resolves.toHaveProperty('controllerKeyId', LTO_KID)
-  await expect(identifier).resolves.toHaveProperty('keys', [
-    {
-      kid: LTO_KID,
-      kms: 'local',
-      type: 'Ed25519',
-      publicKeyHex: PUBLIC_KEY_HEX,
-    },
-  ])
-  return expect(identifier).resolves.toHaveProperty('services', [])
-  */
