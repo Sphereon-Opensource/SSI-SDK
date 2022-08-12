5816fca520dddeb6451d4943f3d266f35914a4c4
nklomp <nklomp@sphereon.com>
2022-08-12 17:57:37 +0200

    chore: Make connection updates working properly

--- packages/data-store-common/src/entities/connection/BaseConfigEntity.ts
+++ packages/data-store-common/src/entities/connection/BaseConfigEntity.ts
@@ -1,6 +1,27 @@
-import { Entity, TableInheritance } from 'typeorm'
-import { AbstractBaseEntity } from './AbstractBaseEntity'
+import { BaseEntity, BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn, TableInheritance } from 'typeorm'
 
 @Entity('BaseConfigEntity')
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
-export abstract class BaseConfigEntity extends AbstractBaseEntity {}
+export abstract class BaseConfigEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @Column({ name: 'created_at', nullable: false })
+  createdAt!: string
+
+  @Column({ name: 'last_updated_at', nullable: false })
+  lastUpdatedAt!: string
+
+  // We are using these hooks instead of the @CreatedDate typorm,
+  // since we have a need for string dates and need it to be compatible across databases
+  @BeforeUpdate()
+  public setUpdatedAt() {
+    this.lastUpdatedAt = new Date().toISOString()
+  }
+
+  @BeforeInsert()
+  public setCreatedAt() {
+    this.createdAt = new Date().toISOString()
+    this.lastUpdatedAt = this.createdAt
+  }
+}
