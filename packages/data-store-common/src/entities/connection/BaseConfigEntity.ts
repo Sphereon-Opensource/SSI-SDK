60586125c846d41593b2f0f00ac72d848e47fa3e
nklomp <nklomp@sphereon.com>
2022-08-12 10:09:19 +0200

    fix: Move audit columns to string and ensure it works on multiple DBs. Needed for react-native state management

--- packages/data-store-common/src/entities/connection/BaseConfigEntity.ts
+++ packages/data-store-common/src/entities/connection/BaseConfigEntity.ts
@@ -1,14 +1,6 @@
-import { BaseEntity, CreateDateColumn, Entity, PrimaryGeneratedColumn, TableInheritance, UpdateDateColumn } from 'typeorm'
+import { Entity, TableInheritance } from 'typeorm'
+import { AbstractBaseEntity } from './AbstractBaseEntity'
 
 @Entity('BaseConfigEntity')
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
-export abstract class BaseConfigEntity extends BaseEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id!: string
-
-  @CreateDateColumn({ name: 'created_at', nullable: false })
-  createdAt!: Date
-
-  @UpdateDateColumn({ name: 'last_updated_at', nullable: false })
-  lastUpdatedAt!: Date
-}
+export abstract class BaseConfigEntity extends AbstractBaseEntity {}
