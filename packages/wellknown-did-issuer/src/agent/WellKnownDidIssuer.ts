6a1757f9de2cf84e341b77c85f6cece2770fb2d1
A.G.J. Cate <brummos@gmail.com>
2022-08-17 12:20:22 +0200

    VDX-114 WIP

--- packages/wellknown-did-issuer/src/agent/WellKnownDidIssuer.ts
+++ packages/wellknown-did-issuer/src/agent/WellKnownDidIssuer.ts
@@ -2,17 +2,21 @@ import { IAgentPlugin, IIdentifier } from '@veramo/core'
 import { WellKnownDidIssuer as Issuer } from '@sphereon/wellknown-dids-client'
 import {
   IDidConfigurationResource,
-  IVerifyCallbackArgs,
-  IVerifyCredentialResult,
+  IIssueCallbackArgs,
+  IIssueDomainLinkageCredentialArgs,
+  ISignedDomainLinkageCredential,
+  ServiceTypesEnum,
 } from '@sphereon/wellknown-dids-client/dist/types'
-import {IIssueDidConfigurationResourceArgs, schema} from '../index'
+import { schema } from '../index'
 import {
   IWellKnownDidIssuer,
   IWellKnownDidIssuerOptionsArgs,
   IRegisterIssueCredentialArgs,
   IRemoveCredentialIssuanceArgs,
   IRequiredContext,
+  IIssueDidConfigurationResourceArgs
 } from '../types/IWellKnownDidIssuer'
+import { Service } from 'did-resolver/lib/resolver';
 
 /**
  * {@inheritDoc IWellKnownDidIssuer}
@@ -23,9 +27,10 @@ export class WellKnownDidIssuer implements IAgentPlugin {
     registerCredentialIssuance: this.registerCredentialIssuance.bind(this),
     removeCredentialIssuance: this.removeCredentialIssuance.bind(this),
     issueDidConfigurationResource: this.issueDidConfigurationResource.bind(this),
+    issueDomainLinkageCredential: this.issueDomainLinkageCredential.bind(this),
   }
 
-  private readonly credentialIssuances: Record<string, (args: IVerifyCallbackArgs) => Promise<IVerifyCredentialResult>>
+  private readonly credentialIssuances: Record<string, (args: IIssueCallbackArgs) => Promise<ISignedDomainLinkageCredential | string>>
 
   constructor(args?: IWellKnownDidIssuerOptionsArgs) {
     this.credentialIssuances = (args && args.credentialIssuances) || {}
@@ -37,7 +42,7 @@ export class WellKnownDidIssuer implements IAgentPlugin {
       return Promise.reject(new Error(`Credential issuance with key: ${args.callbackName} already present`))
     }
 
-    this.credentialIssuances[args.callbackName] = args.signatureVerification
+    this.credentialIssuances[args.callbackName] = args.credentialIssuance
   }
 
   /** {@inheritDoc IWellKnownDidIssuer.removeSignatureVerification} */
@@ -46,26 +51,58 @@ export class WellKnownDidIssuer implements IAgentPlugin {
   }
 
   /** {@inheritDoc IWellKnownDidIssuer.issueDidConfigurationResource} */
-  private async issueDidConfigurationResource(args: IIssueDidConfigurationResourceArgs, context: IRequiredContext): Promise<IDidConfigurationResource> {
+  private async issueDidConfigurationResource(args: IIssueDidConfigurationResourceArgs, context: IRequiredContext): Promise<IDidConfigurationResource> { //IDidConfigurationResource
+    // if (new URL(args.origin).origin !== args.origin) {
+    //   return Promise.reject(Error('Origin is not valid'))
+    // }
+    //
+    // if (new URL(args.origin).protocol !== 'https:') {
+    //   return Promise.reject('Origin is not secure')
+    // }
+
+    // TODO check if any origin is suplied
+
+    // TODO do i check if there is already a service for the given origin?
 
     // TODO parse did
 
     return context.agent.didManagerGet({ did: args.did })
-      .then((identifier: IIdentifier) => {
-
+      .catch(() => Promise.reject(Error('DID cannot be found')))
+      .then(async (identifier: IIdentifier) => {
+        if (!identifier.services || identifier.services.filter((service: Service) => service.type = ServiceTypesEnum.LINKED_DOMAINS).length === 0) {
+          await context.agent.didManagerAddService({
+            did: identifier.did,
+            service: {
+              id: identifier.did, // TODO uuid?
+              type: ServiceTypesEnum.LINKED_DOMAINS,
+              serviceEndpoint: (args.origins.length < 2)
+                ? args.origins[0]
+                : { origins: args.origins }// TODO could be multiple in string array
+            }
+          })
+        }
       })
-      .catch((error) => console.log(error))
-
-
-
-    // return new Issuer().issueDidConfigurationResource({
-    //   verifySignatureCallback: signatureVerification,
-    //   configuration: args.configuration,
-    //   origin: args.origin,
-    //   did: args.did,
-    // })
+      .catch((error: Error) => Promise.reject(Error(`Unable to add LinkedDomains service to DID. Error: ${error.message}`)))
+      .then(() =>
+          new Issuer().issueDidConfigurationResource({
+            issuances: [{
+              did: args.did,
+              origin: args.origin,
+              expirationDate: args.expirationDate,
+              options: args.options
+            }]
+          })
+      )
+      .catch((error: Error) => Promise.reject(Error(`Unable to issue DID configuration resource. Error: ${error.message}`)))
   }
 
-
+  public async issueDomainLinkageCredential(args: IIssueDomainLinkageCredentialArgs): Promise<ISignedDomainLinkageCredential | string> {
+    return new Issuer().issueDomainLinkageCredential({
+      did: args.did,
+      origin: args.origin,
+      expirationDate: args.expirationDate,
+      options: args.options
+    })
+  }
 
 }
--- packages/wellknown-did-issuer/src/types/IWellKnownDidIssuer.ts
+++ packages/wellknown-did-issuer/src/types/IWellKnownDidIssuer.ts
@@ -1,24 +1,25 @@
 import {
   IDidConfigurationResource,
-  IVerifyCallbackArgs,
-  IVerifyCredentialResult,
+  IIssueCallbackArgs,
+  IIssueDomainLinkageCredentialArgs,
+  ISignedDomainLinkageCredential,
 } from '@sphereon/wellknown-dids-client/dist/types'
 import { IAgentContext, IPluginMethodMap, IDIDManager, IKeyManager } from '@veramo/core'
 
 export interface IWellKnownDidIssuer extends IPluginMethodMap {
   registerCredentialIssuance(args: IRegisterIssueCredentialArgs, context: IRequiredContext): Promise<void>,
   removeCredentialIssuance(args: IRemoveCredentialIssuanceArgs, context: IRequiredContext): Promise<boolean>,
-  issueDidConfigurationResource(args: IIssueDidConfigurationResourceArgs, context: IRequiredContext): Promise<IDidConfigurationResource>
+  issueDidConfigurationResource(args: IIssueDidConfigurationResourceArgs, context: IRequiredContext): Promise<IDidConfigurationResource>,
+  issueDomainLinkageCredential(args: IIssueDomainLinkageCredentialArgs): Promise<ISignedDomainLinkageCredential | string>
 }
 
 export interface IWellKnownDidIssuerOptionsArgs {
-  credentialIssuances?: Record<string, (args: IVerifyCallbackArgs) => Promise<IVerifyCredentialResult>>
-  onlyVerifyServiceDids?: boolean
+  credentialIssuances?: Record<string, (args: IIssueCallbackArgs) => Promise<ISignedDomainLinkageCredential | string>>
 }
 
 export interface IRegisterIssueCredentialArgs {
   callbackName: string
-  signatureVerification: (args: IVerifyCallbackArgs) => Promise<IVerifyCredentialResult>
+  credentialIssuance: (args: IIssueCallbackArgs) => Promise<ISignedDomainLinkageCredential | string>
 }
 
 export interface IRemoveCredentialIssuanceArgs {
@@ -27,11 +28,8 @@ export interface IRemoveCredentialIssuanceArgs {
 
 export interface IIssueDidConfigurationResourceArgs {
   did: string
+  origins: Array<string>
+  issueCallback?: (args: IIssueCallbackArgs) => Promise<ISignedDomainLinkageCredential | string>
 }
 
-
-
-
-
-
-export type IRequiredContext = IAgentContext<Pick<IDIDManager, 'didManagerGet'> & Pick<IKeyManager, 'keyManagerGet' | 'keyManagerSign'>>
+export type IRequiredContext = IAgentContext<Pick<IDIDManager, 'didManagerGet' | 'didManagerAddService'> & Pick<IKeyManager, 'keyManagerGet' | 'keyManagerSign'>>
