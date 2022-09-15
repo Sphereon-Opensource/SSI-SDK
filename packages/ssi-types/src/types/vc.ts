e8c9e783b4a8b61a176213a8e465cf18db2f63d6
Maikel Maas <mmaas@sphereon.com>
2022-09-15 10:52:39 +0200

    VDX-135 Verify the VC signatures of linked domain
       - Made id optional in the credential;

--- packages/ssi-types/src/types/vc.ts
+++ packages/ssi-types/src/types/vc.ts
@@ -11,7 +11,7 @@ export interface ICredential {
   // If sub is present, the value MUST be used to set the value of the id property of credentialSubject of the new credential JSON object.
   credentialSubject: ICredentialSubject
   // If jti is present, the value MUST be used to set the value of the id property of the new JSON object.
-  id: string
+  id?: string
   '@context': ICredentialContextType[] | ICredentialContextType
   credentialStatus?: ICredentialStatus
   credentialSchema?: undefined | ICredentialSchemaType | ICredentialSchemaType[]
