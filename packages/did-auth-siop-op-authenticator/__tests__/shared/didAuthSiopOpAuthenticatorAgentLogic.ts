f207f161c3b02442fabc1f5f8e56700fc1edf784
Maikel Maas <mmaas@sphereon.com>
2022-11-03 15:12:03 +0100

    WAL-223 Fix credential matching in did-auth-siop-authenticator

--- packages/did-auth-siop-op-authenticator/__tests__/shared/didAuthSiopOpAuthenticatorAgentLogic.ts
+++ packages/did-auth-siop-op-authenticator/__tests__/shared/didAuthSiopOpAuthenticatorAgentLogic.ts
@@ -1,5 +1,5 @@
 import { TAgent } from '@veramo/core'
-import { OP, PresentationExchange } from '@sphereon/did-auth-siop'
+import { OP } from '@sphereon/did-auth-siop'
 import { IDidAuthSiopOpAuthenticator, IMatchedPresentationDefinition } from '../../src/types/IDidAuthSiopOpAuthenticator'
 
 import {
@@ -175,14 +175,6 @@ export default (testContext: {
       const mockSubmitAuthenticationResponseMethod = jest.fn()
       OP.prototype.submitAuthenticationResponse = mockSubmitAuthenticationResponseMethod
       mockSubmitAuthenticationResponseMethod.mockReturnValue(Promise.resolve({ status: 200, statusText: 'example_value' }))
-
-      const mockedSelectVerifiableCredentialsForSubmissionMethod = jest.fn()
-      PresentationExchange.prototype.selectVerifiableCredentialsForSubmission = mockedSelectVerifiableCredentialsForSubmissionMethod
-      mockedSelectVerifiableCredentialsForSubmissionMethod.mockReturnValue(Promise.resolve({ errors: [], matches: ['match'] }))
-
-      const mockedSubmissionFromMethod = jest.fn()
-      PresentationExchange.prototype.submissionFrom = mockedSubmissionFromMethod
-      mockedSubmissionFromMethod.mockReturnValue(Promise.resolve({}))
 
       await agent.registerSessionForSiop({
         sessionId,
@@ -361,10 +353,9 @@ export default (testContext: {
                 fields: [
                   {
                     path: ['$.issuer.id'],
-                    purpose: 'We can only verify bank accounts if they are attested by a source.',
                     filter: {
                       type: 'string',
-                      pattern: 'did:example:issuer',
+                      _const: 'did:example:issuer'
                     },
                   },
                 ],
@@ -400,13 +391,99 @@ export default (testContext: {
           jws: 'eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..kTCYt5XsITJX1CxPCT8yAV-TVIw5WEuts01mq-pQy7UJiN5mgREEMGlv50aqzpqh4Qq_PbChOMqsLfRoPsnsgxD-WUcX16dUOqV0G_zS245-kronKb78cPktb3rk-BuQy72IFLN25DYuNzVBAh4vGHSrQyHUGlcTwLtjPAnKb78',
         },
       },
+      {
+        id: 'https://example.com/credentials/1873',
+        type: ['VerifiableCredential', 'DriversLicense'],
+        '@context': ['https://www.w3.org/2018/credentials/v1', 'https://www.w3.org/2018/credentials/examples/v1/DriversLicense'],
+        issuer: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+        issuanceDate: '2010-01-01T19:23:24Z',
+        credentialSubject: {
+          given_name: 'John',
+          family_name: 'Doe',
+          birthdate: '1975-01-05',
+        },
+        proof: {
+          type: 'RsaSignature2018',
+          created: '2018-09-14T21:19:10Z',
+          proofPurpose: 'authentication',
+          verificationMethod: 'did:example:ebfeb1f712ebc6f1c276e12ec21#keys-1',
+          challenge: '1f44d55f-f161-4938-a659-f8026467f126',
+          domain: '4jt78h47fh47',
+          jws: 'eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..kTCYt5XsITJX1CxPCT8yAV-TVIw5WEuts01mq-pQy7UJiN5mgREEMGlv50aqzpqh4Qq_PbChOMqsLfRoPsnsgxD-WUcX16dUOqV0G_zS245-kronKb78cPktb3rk-BuQy72IFLN25DYuNzVBAh4vGHSrQyHUGlcTwLtjPAnKb78',
+        },
+      },
     ]
 
+      const vps =  [
+        {
+          "format": "ldp_vp",
+          "location": "id_token",
+          "presentation": {
+            "@context": [
+              "https://www.w3.org/2018/credentials/v1",
+              "https://identity.foundation/presentation-exchange/submission/v1"
+            ],
+            "holder": undefined, //needs to be fixed
+            "presentation_submission": {
+              "definition_id": "Credentials",
+              "descriptor_map": [
+                {
+                  "format": "ldp_vc",
+                  "id": "ID Card Credential",
+                  "path": "$.verifiableCredential[0]"
+                }
+              ],
+              "id": expect.any(String)
+            },
+            "type": [
+              "VerifiablePresentation",
+              "PresentationSubmission"
+            ],
+            "verifiableCredential": [
+              {
+                "@context": [
+                  "https://www.w3.org/2018/credentials/v1",
+                  "https://www.w3.org/2018/credentials/examples/v1/IDCardCredential"
+                ],
+                "credentialSubject": {
+                  "birthdate": "1949-01-22",
+                  "family_name": "Stremberg",
+                  "given_name": "Fredrik"
+                },
+                "id": "https://example.com/credentials/1872",
+                "issuanceDate": "2010-01-01T19:23:24Z",
+                "issuer": {
+                  "id": "did:example:issuer"
+                },
+                "proof": {
+                  "challenge": "1f44d55f-f161-4938-a659-f8026467f126",
+                  "created": "2018-09-14T21:19:10Z",
+                  "domain": "4jt78h47fh47",
+                  "jws": "eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..kTCYt5XsITJX1CxPCT8yAV-TVIw5WEuts01mq-pQy7UJiN5mgREEMGlv50aqzpqh4Qq_PbChOMqsLfRoPsnsgxD-WUcX16dUOqV0G_zS245-kronKb78cPktb3rk-BuQy72IFLN25DYuNzVBAh4vGHSrQyHUGlcTwLtjPAnKb78",
+                  "proofPurpose": "authentication",
+                  "type": "RsaSignature2018",
+                  "verificationMethod": "did:example:ebfeb1f712ebc6f1c276e12ec21#keys-1"
+                },
+                "type": [
+                  "VerifiableCredential",
+                  "IDCardCredential"
+                ]
+              }
+            ]
+          }
+        }
+        ]
     it('should match single vc', async () => {
       const sessionId = 'new_session_id'
       const session: OpSession = await agent.getSessionForSiop({ sessionId })
       const result: IMatchedPresentationDefinition[] = await session.matchPresentationDefinitions(pds, vcs)
-      expect(result).toEqual([])
+      expect(result).toEqual(vps)
+    })
+    it('should match multiple vcs', async () => {
+      const sessionId = 'new_session_id'
+      const session: OpSession = await agent.getSessionForSiop({ sessionId })
+      const result: IMatchedPresentationDefinition[] = await session.matchPresentationDefinitions(pds, vcs)
+      expect(result).toEqual(vps)
     })
   })
 }
