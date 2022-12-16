548ee874c5d3ed90a4776fa0333203a046b3bc95
nklomp <nklomp@sphereon.com>
2022-12-16 22:46:00 +0100

    update: Node16 support

--- packages/bls-kms-local/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { TKeyType } from '@veramo/core'
-export { BlsKeyManagementSystem } from './BlsKeyManagementSystem'
-export type BlsManagedKeyInfoArgs = { alias?: string; type: TKeyType; privateKeyHex: string; publicKeyHex: string }
-export enum KeyType {
-  Bls12381G2 = 'Bls12381G2',
-}
