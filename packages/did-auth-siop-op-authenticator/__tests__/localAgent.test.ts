65a3191db31c3d5269a6ab4e4f10b5beef8b48d2
Bram ten Cate <btencate@sphereon.com>
2023-01-12 13:31:07 +0100

    WAL-142 code cleanup

--- packages/did-auth-siop-op-authenticator/__tests__/localAgent.test.ts
+++ packages/did-auth-siop-op-authenticator/__tests__/localAgent.test.ts
@@ -1,25 +1,31 @@
+import * as fs from 'fs'
 import { getConfig } from '@veramo/cli/build/setup'
 import { createObjects } from '@veramo/cli/build/lib/objectCreator'
-
-jest.setTimeout(30000)
-
 import didAuthSiopOpAuthenticatorAgentLogic from './shared/didAuthSiopOpAuthenticatorAgentLogic'
 import { PresentationSignCallback } from '@sphereon/did-auth-siop'
 
+jest.setTimeout(30000)
+
+function getFile(path: string) {
+  return fs.readFileSync(path, 'utf-8')
+}
+
+function getFileAsJson(path: string) {
+  return JSON.parse(getFile(path))
+}
+
 let agent: any
 
-const presentationSignCallback: PresentationSignCallback = async (args) => ({
-  ...args.presentation,
-  proof: {
-    type: 'RsaSignature2018',
-    created: '2018-09-14T21:19:10Z',
-    proofPurpose: 'authentication',
-    verificationMethod: 'did:example:ebfeb1f712ebc6f1c276e12ec21#keys-1',
-    challenge: '1f44d55f-f161-4938-a659-f8026467f126',
-    domain: '4jt78h47fh47',
-    jws: 'eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..kTCYt5XsITJX1CxPCT8yAV-TVIw5WEuts01mq-pQy7UJiN5mgREEMGlv50aqzpqh4Qq_PbChOMqsLfRoPsnsgxD-WUcX16dUOqV0G_zS245-kronKb78cPktb3rk-BuQy72IFLN25DYuNzVBAh4vGHSrQyHUGlcTwLtjPAnKb78'
+const presentationSignCallback: PresentationSignCallback = async (args) => {
+  const presentationSignProof = getFileAsJson(
+      './packages/did-auth-siop-op-authenticator/__tests__/vc_vp_examples/psc/psc.json'
+  )
+
+  return {
+    ...args.presentation,
+    ...presentationSignProof
   }
-})
+}
 
 const setup = async (): Promise<boolean> => {
   const config = getConfig('packages/did-auth-siop-op-authenticator/agent.yml')
