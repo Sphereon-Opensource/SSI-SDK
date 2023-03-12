1907345725090a90ae9b96104114f33bc4ed8250
nklomp <nklomp@sphereon.com>
2023-03-12 06:16:11 +0100

    chore: fix exports

--- packages/vc-handler-ld-local/src/index.ts
+++ packages/vc-handler-ld-local/src/index.ts
@@ -3,17 +3,11 @@
  */
 const schema = require('../plugin.schema.json')
 export { schema }
-export {
-  SphereonBbsBlsSignature2020,
-  SphereonEd25519Signature2018,
-  SphereonEd25519Signature2020,
-  SphereonEcdsaSecp256k1RecoverySignature2020,
-  SphereonJsonWebSignature2020,
-} from './suites'
+export * from './suites'
 export * from './ld-credential-module'
 export * from './ld-context-loader'
 export * from './ld-suite-loader'
 export * from './ld-default-contexts'
 export * from './ld-document-loader'
-export { CredentialHandlerLDLocal } from './agent/CredentialHandlerLDLocal'
-export * from './types/ICredentialHandlerLDLocal'
+export * from './agent'
+export * from './types'
--- packages/vc-handler-ld-local/src/types/ICredentialHandlerLDLocal.ts
+++ packages/vc-handler-ld-local/src/types/ICredentialHandlerLDLocal.ts
@@ -1,15 +1,82 @@
 import { VerifiableCredentialSP, VerifiablePresentationSP } from '@sphereon/ssi-sdk-core'
-import { IAgentContext, IDIDManager, IKeyManager, IPluginMethodMap, IResolver } from '@veramo/core'
+import { IPluginMethodMap } from '@veramo/core'
 
-import { ICreateVerifiableCredentialLDArgs, ICreateVerifiablePresentationLDArgs, IVerifyCredentialLDArgs, IVerifyPresentationLDArgs } from './types'
+import {
+  ICreateVerifiableCredentialLDArgs,
+  ICreateVerifiablePresentationLDArgs,
+  IRequiredContext,
+  IVerifyCredentialLDArgs,
+  IVerifyPresentationLDArgs,
+} from './types'
 
+/**
+ * The interface definition for a plugin that can issue and verify Verifiable Credentials and Presentations
+ * that use JSON-LD format.
+ *
+ * @remarks Please see {@link https://www.w3.org/TR/vc-data-model | W3C Verifiable Credentials data model}
+ *
+ * @beta This API is likely to change without a BREAKING CHANGE notice
+ */
 export interface ICredentialHandlerLDLocal extends IPluginMethodMap {
+
+  /**
+   * Creates a Verifiable Credential.
+   * The payload, signer and format are chosen based on the `args` parameter.
+   *
+   * @param args - Arguments necessary to create the Presentation.
+   * @param context - This reserved param is automatically added and handled by the framework, *do not override*
+   *
+   * @returns - a promise that resolves to the {@link @veramo/core#VerifiableCredential} that was requested or rejects with an error
+   * if there was a problem with the input or while getting the key to sign
+   *
+   * @remarks Please see {@link https://www.w3.org/TR/vc-data-model/#credentials | Verifiable Credential data model}
+   *
+   * @beta This API is likely to change without a BREAKING CHANGE notice
+   */
   createVerifiableCredentialLDLocal(args: ICreateVerifiableCredentialLDArgs, context: IRequiredContext): Promise<VerifiableCredentialSP>
 
+  /**
+   * Creates a Verifiable Presentation.
+   * The payload, signer and format are chosen based on the `args` parameter.
+   *
+   * @param args - Arguments necessary to create the Presentation.
+   * @param context - This reserved param is automatically added and handled by the framework, *do not override*
+   *
+   * @returns - a promise that resolves to the {@link @veramo/core#VerifiablePresentation} that was requested or rejects with an error
+   * if there was a problem with the input or while getting the key to sign
+   *
+   * @remarks Please see {@link https://www.w3.org/TR/vc-data-model/#presentations | Verifiable Presentation data model }
+   *
+   * @beta This API is likely to change without a BREAKING CHANGE notice
+   */
   createVerifiablePresentationLDLocal(args: ICreateVerifiablePresentationLDArgs, context: IRequiredContext): Promise<VerifiablePresentationSP>
 
+  /**
+   * Verifies a Verifiable Credential JWT or LDS Format.
+   *
+   * @param args - Arguments necessary to verify a VerifiableCredential
+   * @param context - This reserved param is automatically added and handled by the framework, *do not override*
+   *
+   * @returns - a promise that resolves to the boolean true on successful verification or rejects on error
+   *
+   * @remarks Please see {@link https://www.w3.org/TR/vc-data-model/#credentials | Verifiable Credential data model}
+   *
+   * @beta This API is likely to change without a BREAKING CHANGE notice
+   */
   verifyCredentialLDLocal(args: IVerifyCredentialLDArgs, context: IRequiredContext): Promise<boolean>
 
+  /**
+   * Verifies a Verifiable Presentation JWT or LDS Format.
+   *
+   * @param args - Arguments necessary to verify a VerifiableCredential
+   * @param context - This reserved param is automatically added and handled by the framework, *do not override*
+   *
+   * @returns - a promise that resolves to the boolean true on successful verification or rejects on error
+   *
+   * @remarks Please see {@link https://www.w3.org/TR/vc-data-model/#presentations | Verifiable Credential data model}
+   *
+   * @beta This API is likely to change without a BREAKING CHANGE notice
+   */
   verifyPresentationLDLocal(args: IVerifyPresentationLDArgs, context: IRequiredContext): Promise<boolean>
 }
 
@@ -33,5 +100,3 @@ export enum events {
   PRESENTATION_VERIFY_FAILED = 'presentationVerificationFailed',
   CREDENTIAL_VERIFY_FAILED = 'credentialVerificationFailed',
 }
-
-export type IRequiredContext = IAgentContext<IResolver & Pick<IDIDManager, 'didManagerGet'> & Pick<IKeyManager, 'keyManagerGet' | 'keyManagerSign'>>
