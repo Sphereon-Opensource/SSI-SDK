d6d782898290d32a63d77c01c805d43153000e3a
Maikel Maas <mmaas@sphereon.com>
2023-03-17 14:24:13 +0100

    VDX-161 fix mnemonic-seed-manager tests
       - Running tests using in memory database;

--- packages/mnemonic-seed-manager/__tests__/localAgent.test.ts
+++ packages/mnemonic-seed-manager/__tests__/localAgent.test.ts
@@ -1,28 +1,25 @@
-import * as fs from 'fs'
-
-import { DataSource } from 'typeorm'
+import {DataSource} from 'typeorm'
 
 import mnemonicGenerator from './shared/generateMnemonic'
 import seedGenerator from './shared/generateSeed'
 import storeSeed from './shared/storeMnemonicInfo'
-import { createAgent, IDataStore, IKeyManager } from '@veramo/core'
-import { KeyManager } from '@veramo/key-manager'
-import { IMnemonicSeedManager, MnemonicSeedManager, MnemonicSeedManagerEntities, MnemonicSeedManagerMigrations } from '../src'
-import { Entities, KeyStore, migrations, PrivateKeyStore } from '@veramo/data-store'
-import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
-import { OrPromise } from '@veramo/utils'
+import {createAgent, IDataStore, IKeyManager} from '@veramo/core'
+import {KeyManager} from '@veramo/key-manager'
+import {IMnemonicSeedManager, MnemonicSeedManager, MnemonicSeedManagerEntities, MnemonicSeedManagerMigrations} from '../src'
+import {Entities, KeyStore, migrations, PrivateKeyStore} from '@veramo/data-store'
+import {KeyManagementSystem, SecretBox} from '@veramo/kms-local'
+import {OrPromise} from '@veramo/utils'
 
 jest.setTimeout(30000)
 
 const KMS_SECRET_KEY = 'd17c8674f5db9396f8eecccde25e882bb0336316bc411ae38dc1f3dcd7ed100f'
-let databaseFile = 'database.sqlite'
 let dbConnection: OrPromise<DataSource>
 let agent: any
 
 const setup = async (): Promise<boolean> => {
   const db: OrPromise<DataSource> = new DataSource({
     type: 'sqlite',
-    database: databaseFile,
+    database: ':memory:',
     synchronize: false,
     logging: false,
     entities: [...MnemonicSeedManagerEntities, ...Entities],
@@ -49,13 +46,12 @@ const setup = async (): Promise<boolean> => {
 
 const tearDown = async (): Promise<boolean> => {
   await (await dbConnection).destroy()
-  fs.unlinkSync(databaseFile)
   return true
 }
 
 const getAgent = () => agent
 
-const testContext = { getAgent, setup, tearDown }
+const testContext = {getAgent, setup, tearDown}
 
 describe('Local integration tests', () => {
   mnemonicGenerator(testContext)
