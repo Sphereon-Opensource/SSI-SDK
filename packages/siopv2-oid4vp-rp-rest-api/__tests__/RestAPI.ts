1a534cd5eab80a1bd697856e4a240fd2f30fa86b
nklomp <nklomp@sphereon.com>
2023-04-13 17:34:22 +0200

    chore: update deps, rename siop rest api module

--- packages/siopv2-oid4vp-rp-rest-api/__tests__/RestAPI.ts
+++ packages/siopv2-oid4vp-rp-rest-api/__tests__/RestAPI.ts
@@ -4,8 +4,8 @@ import { ISIOPv2RPRestAPIOpts, SIOPv2RPRestAPI } from '../src'
 const opts: ISIOPv2RPRestAPIOpts = {
   hostname: '0.0.0.0',
   port: 5000,
-  webappBaseURI: 'http://172.16.255.33:5000',
-  siopBaseURI: 'http://172.16.255.33:5000',
+  webappBaseURI: 'http://192.168.2.18:5000',
+  siopBaseURI: 'http://192.168.2.18:5000',
 }
 
 new SIOPv2RPRestAPI({ agent, opts })
