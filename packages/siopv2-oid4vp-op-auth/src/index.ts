6f3a76c592a31dbf0c4d352bb55933372932ae33
nklomp <nklomp@sphereon.com>
2023-05-03 16:35:47 +0200

    chore: fix exports

--- packages/siopv2-oid4vp-op-auth/src/index.ts
+++ packages/siopv2-oid4vp-op-auth/src/index.ts
@@ -4,6 +4,5 @@
 const schema = require('../plugin.schema.json')
 export { schema }
 export { DidAuthSiopOpAuthenticator } from './agent/DidAuthSiopOpAuthenticator'
-export { OpSession } from './session/OpSession'
-export { OID4VP } from './session/OID4VP'
+export * from './session'
 export * from './types/IDidAuthSiopOpAuthenticator'
