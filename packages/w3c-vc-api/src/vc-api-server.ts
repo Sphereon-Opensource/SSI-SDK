d02014ad162f657caf0779f5d46e92073d8e660d
Niels Klomp <nklomp@sphereon.com>
2023-07-25 01:37:56 +0200

    feat:VC API support for authentication and authorization

--- packages/w3c-vc-api/src/vc-api-server.ts
+++ packages/w3c-vc-api/src/vc-api-server.ts
@@ -1,9 +1,8 @@
 import { agentContext } from '@sphereon/ssi-sdk.core'
+import { ExpressBuildResult } from '@sphereon/ssi-sdk.express-support'
 import { TAgent } from '@veramo/core'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import * as dotenv from 'dotenv-flow'
-import express, { Express } from 'express'
+
+import express, { Express, Router } from 'express'
 import {
   deleteCredentialEndpoint,
   getCredentialEndpoint,
@@ -11,36 +10,61 @@ import {
   issueCredentialEndpoint,
   verifyCredentialEndpoint,
 } from './api-functions'
-import { IRequiredPlugins, IVCAPIOpts } from './types'
+import { IRequiredPlugins, IVCAPIOpts, IVCIApiFeatures } from './types'
 
 export class VcApiServer {
+  get router(): express.Router {
+    return this._router
+  }
+
   private readonly _express: Express
   private readonly _agent: TAgent<IRequiredPlugins>
   private readonly _opts?: IVCAPIOpts
+  private readonly _router: Router
 
-  constructor(args: { agent: TAgent<IRequiredPlugins>; express?: Express; opts?: IVCAPIOpts }) {
+  constructor(args: { agent: TAgent<IRequiredPlugins>; expressArgs: ExpressBuildResult; opts?: IVCAPIOpts }) {
     const { agent, opts } = args
     this._agent = agent
+    if (opts?.endpointOpts?.globalAuth) {
+      copyGlobalAuthToEndpoint(opts, 'issueCredential')
+      copyGlobalAuthToEndpoint(opts, 'getCredential')
+      copyGlobalAuthToEndpoint(opts, 'getCredentials')
+      copyGlobalAuthToEndpoint(opts, 'deleteCredential')
+      copyGlobalAuthToEndpoint(opts, 'verifyCredential')
+    }
+
     this._opts = opts
-    const existingExpress = !!args.express
-    this._express = args.express ?? express()
-    this.setupExpress(existingExpress)
-    const router = express.Router()
+    this._express = args.expressArgs.express
+    this._router = express.Router()
+
     const context = agentContext(agent)
 
+    const features = opts?.issueCredentialOpts?.enableFeatures ?? [
+      IVCIApiFeatures.VC_ISSUE,
+      IVCIApiFeatures.VC_PERSISTENCE,
+      IVCIApiFeatures.VC_VERIFY,
+    ]
+    console.log(`VC API enabled, with features: ${JSON.stringify(features)}`)
+
     // Credential endpoints
-    issueCredentialEndpoint(router, context, {
-      issueCredentialOpts: opts?.issueCredentialOpts,
-      issueCredentialPath: opts?.pathOpts?.issueCredentialPath,
-    })
-    getCredentialEndpoint(router, context, { getCredentialPath: opts?.pathOpts?.getCredentialPath })
-    getCredentialsEndpoint(router, context, { getCredentialsPath: opts?.pathOpts?.getCredentialsPath })
-    deleteCredentialEndpoint(router, context, { deleteCredentialsPath: opts?.pathOpts?.deleteCredentialPath }) // not in spec. TODO: Authz
-    verifyCredentialEndpoint(router, context, {
-      verifyCredentialPath: opts?.pathOpts?.verifyCredentialPath,
-      fetchRemoteContexts: opts?.issueCredentialOpts?.fetchRemoteContexts,
-    })
-    this._express.use(opts?.pathOpts?.basePath ?? '', router)
+    if (features.includes(IVCIApiFeatures.VC_ISSUE)) {
+      issueCredentialEndpoint(this.router, context, {
+        ...opts?.endpointOpts?.issueCredential,
+        issueCredentialOpts: opts?.issueCredentialOpts,
+      })
+    }
+    if (features.includes(IVCIApiFeatures.VC_PERSISTENCE)) {
+      getCredentialEndpoint(this.router, context, opts?.endpointOpts?.getCredential)
+      getCredentialsEndpoint(this.router, context, opts?.endpointOpts?.getCredentials)
+      deleteCredentialEndpoint(this.router, context, opts?.endpointOpts?.deleteCredential) // not in spec.
+    }
+    if (features.includes(IVCIApiFeatures.VC_VERIFY)) {
+      verifyCredentialEndpoint(this.router, context, {
+        ...opts?.endpointOpts?.verifyCredential,
+        fetchRemoteContexts: opts?.issueCredentialOpts?.fetchRemoteContexts,
+      })
+    }
+    this._express.use(opts?.endpointOpts?.basePath ?? '', this.router)
   }
 
   get agent(): TAgent<IRequiredPlugins> {
@@ -54,32 +78,16 @@ export class VcApiServer {
   get express(): Express {
     return this._express
   }
+}
 
-  private setupExpress(existingExpress: boolean) {
-    dotenv.config()
-    if (!existingExpress) {
-      const port = this.opts?.serverOpts?.port || process.env.PORT || 5000
-      const secret = this.opts?.serverOpts?.cookieSigningKey || process.env.COOKIE_SIGNING_KEY
-      const hostname = this.opts?.serverOpts?.hostname || '0.0.0.0'
-      this._express.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*')
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', 'true')
-        next()
-      })
-      // this.express.use(cors({ credentials: true }));
-      // this.express.use('/proxy', proxy('www.gssoogle.com'));
-      this._express.use(bodyParser.urlencoded({ extended: true }))
-      this._express.use(bodyParser.json())
-      this._express.use(cookieParser(secret))
-      this._express.listen(port as number, hostname, () => console.log(`Listening on ${hostname}, port ${port}`))
+function copyGlobalAuthToEndpoint(opts: IVCAPIOpts, key: string) {
+  if (opts?.endpointOpts?.globalAuth) {
+    // @ts-ignore
+    opts.endpointOpts[key] = {
+      // @ts-ignore
+      ...opts.endpointOpts[key],
+      // @ts-ignore
+      endpoint: { ...opts.endpointOpts.globalAuth, ...opts.endpointOpts[key]?.endpoint },
     }
   }
 }
