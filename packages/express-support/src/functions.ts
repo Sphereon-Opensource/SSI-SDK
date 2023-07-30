4151c73b4cdeb931c0deb8b8f34ed9c215efe5ba
Niels Klomp <nklomp@sphereon.com>
2023-07-30 23:17:05 +0200

    feat: Allow objects for error response. Improve json handling in error responses

--- packages/express-support/src/functions.ts
+++ packages/express-support/src/functions.ts
@@ -1,25 +1,31 @@
-import express, { NextFunction } from 'express'
+import express, {NextFunction} from 'express'
 import process from 'process'
 
 export function env(key?: string, prefix?: string): string | undefined {
-  if (!key) {
-    return
-  }
-  return process.env[`${prefix ? prefix.trim() : ''}${key}`]
+    if (!key) {
+        return
+    }
+    return process.env[`${prefix ? prefix.trim() : ''}${key}`]
 }
 
-export function sendErrorResponse(response: express.Response, statusCode: number, message: string, error?: Error) {
-  console.log(message)
-  if (error) {
-    console.log(error)
-  }
-  response.statusCode = statusCode
-  return response.status(statusCode).json(message.startsWith('{') ? message : JSON.stringify(message, null, 2))
+export function sendErrorResponse(response: express.Response, statusCode: number, message: string | object, error?: Error) {
+    console.log(message)
+    if (error) {
+        console.log(error)
+    }
+    response.statusCode = statusCode
+    if (typeof message === 'string' && !message.startsWith('{')) {
+        message = {error: message}
+    }
+    if (typeof message === 'string' && message.startsWith('{')) {
+        return response.status(statusCode).end(message)
+    }
+    return response.status(statusCode).json(message)
 }
 
 export const jsonErrorHandler = (err: any, req: express.Request, res: express.Response, next: NextFunction) => {
-  if (res.headersSent) {
-    return next(err)
-  }
-  return sendErrorResponse(res, 500, err.message, err)
+    if (res.headersSent) {
+        return next(err)
+    }
+    return sendErrorResponse(res, 500, err.message, err)
 }
