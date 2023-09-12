20ca9d0b86011ece601ccd68a57f60992180fd53
sksadjad <skhoshkhou@sphereon.com>
2023-09-12 21:13:02 +0200

    chore: disabled authentication for contact-manager-rest-api test

--- packages/contact-manager-rest-api/__tests__/RestAPI.ts
+++ packages/contact-manager-rest-api/__tests__/RestAPI.ts
@@ -1,21 +1,13 @@
-import { ExpressBuilder, StaticBearerAuth } from '../../ssi-express-support/src'
+import { ExpressBuilder } from '../../ssi-express-support/src'
 import { ContactManagerApiServer } from '../src'
 import agent from './agent'
-
-StaticBearerAuth.init('bearer')
-  .withUsers([
-    { id: 1, token: '123456', name: 'API User 1' },
-    { id: 2, token: 'abcdef', name: 'API User 2' },
-  ])
-  .connectPassport()
 
 const builder = ExpressBuilder.fromServerOpts({
   port: 5000,
   hostname: '0.0.0.0',
 })
   .withMorganLogging({ format: 'dev' })
-  .withPassportAuth(true)
-  .withSessionOptions({ secret: '1234', name: 'oidc-session' })
+  .withPassportAuth(false)
 
 const expressSupport = builder.build({ startListening: true })
 
@@ -27,18 +19,6 @@ new ContactManagerApiServer({
           enabled: false,
         },
       },
-      partyRead: {
-        disableGlobalAuth: true
-      },
-      partyWrite: {
-        disableGlobalAuth: true
-      },
-      identityRead: {
-        disableGlobalAuth: true
-      },
-      partyTypeRead: {
-        disableGlobalAuth: true
-      }
     },
     enableFeatures: ['party_read', 'party_write', 'party_type_read', 'identity_read'],
   },
