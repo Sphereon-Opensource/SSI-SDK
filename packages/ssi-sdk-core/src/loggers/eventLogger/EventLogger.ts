ec55cccaa3f94afdf76663a595c0888433cd33c3
A.G.J. Cate <brummos@gmail.com>
2023-12-08 12:53:57 +0100

    DPP-96 added missing file

---
+++ packages/ssi-sdk-core/src/loggers/eventLogger/EventLogger.ts
@@ -0,0 +1,26 @@
+import { IAgentContext } from '@veramo/core'
+import { EventLoggerArgs, LoggingEvent } from '../../types'
+import Debug, { Debugger } from 'debug'
+
+class EventLogger {
+  private readonly context?: IAgentContext<any>;
+  private readonly namespace?: string
+  private readonly debug: Debugger
+
+  constructor(args: EventLoggerArgs) {
+    const { context, namespace } = args
+    this.context = context
+    this.namespace = namespace
+    this.debug = Debug(this.namespace ?? 'sphereon:ssi-sdk:EventLogger')
+  }
+
+  public logEvent = async (event: LoggingEvent): Promise<void> => {
+    this.debug('logging event:', event);
+    if (this.context?.agent) {
+      await this.context.agent.emit(event.type, event.data)
+    }
+  }
+
+}
+
+export default EventLogger;
