7e6c4b9631eab59bbec6898856a31d556a8119e6
Maikel Maas <mmaas@sphereon.com>
2024-02-19 11:20:36 -0300

    chore: Added @ts-ignore to entity file

--- packages/data-store/src/entities/xstatePersistence/XStateEntity.ts
+++ packages/data-store/src/entities/xstatePersistence/XStateEntity.ts
@@ -3,19 +3,27 @@ import {BaseEntity, Column, Entity, PrimaryColumn} from "typeorm";
 @Entity('XstateEntity')
 export class XStateEntity extends BaseEntity {
     @PrimaryColumn({ name: 'id', type: 'varchar' })
+    // @ts-ignore
     id: string
     @Column()
+    // @ts-ignore
     state: string
     @Column()
+    // @ts-ignore
     type: string
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    // @ts-ignore
     createdAt: Date
     @Column({ type: 'timestamp', onUpdate: 'CURRENT_TIMESTAMP', nullable: true })
+    // @ts-ignore
     updatedAt: Date
     @Column({ type: 'timestamp', nullable: true })
+    // @ts-ignore
     completedAt: Date
     @Column()
+    // @ts-ignore
     tenantId?: string
     @Column({ default: 0 })
+    // @ts-ignore
     ttl: number
 }
