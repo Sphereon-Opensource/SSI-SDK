bddad9ff76680735c0bff6f393c42dc07375d5d6
Maikel Maas <mmaas@sphereon.com>
2024-02-19 11:38:04 -0300

    chore: Fixed now function in migration file

--- packages/data-store/src/migrations/sqlite/1708096002272-CreateXStateStore.ts
+++ packages/data-store/src/migrations/sqlite/1708096002272-CreateXStateStore.ts
@@ -5,12 +5,12 @@ export class CreateXStateStore1708096002272 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(
-            `CREATE TABLE "XStateStore" ("id" varchar PRIMARY KEY NOT NULL, "state" varchar(255) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "PK_XStateStore_id")`
+            `CREATE TABLE "XStateEntity" ("id" varchar PRIMARY KEY NOT NULL, "state" varchar(255) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT (datetime('now')), "updatedAt" TIMESTAMP NOT NULL DEFAULT (datetime('now')), "deletedAt" TIMESTAMP, CONSTRAINT "PK_XStateEntity_id")`
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "XStateStore" DROP CONSTRAINT "PK_XStateStore_id"`)
-        await queryRunner.query(`DROP TABLE "XStateStore"`)
+        await queryRunner.query(`ALTER TABLE "XStateEntity" DROP CONSTRAINT "PK_XStateEntity_id"`)
+        await queryRunner.query(`DROP TABLE "XStateEntity"`)
     }
 }
