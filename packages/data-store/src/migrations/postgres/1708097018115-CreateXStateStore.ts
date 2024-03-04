737de6f386c77d39ca2b78c860e2b6038e067056
sksadjad <skhoshkhou@sphereon.com>
2024-03-04 23:17:15 +0100

    refactor: updated xstate migrations

--- packages/data-store/src/migrations/postgres/1708097018115-CreateXStateStore.ts
+++ packages/data-store/src/migrations/postgres/1708097018115-CreateXStateStore.ts
@@ -4,15 +4,26 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class CreateXStateStore1708097018115 implements MigrationInterface {
   name = 'CreateXStateStore1708097018115'
 
-  public async up(queryRunner: QueryRunner): Promise<any> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
     await enablePostgresUuidExtension(queryRunner)
-    await queryRunner.query(
-      `CREATE TABLE "StateEntity" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "state" varchar(255) NOT NULL, "type" varchar(255) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "completed_at" TIMESTAMP, "tenant_id" varchar(255) NULL, "ttl" INTEGER NULL, CONSTRAINT PK_XStateStore_id PRIMARY KEY ("id"))`
-    )
+    await queryRunner.query(`
+            CREATE TABLE "StateEntity" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "step" character varying NOT NULL,
+                "type" character varying NOT NULL,
+                "event_name" character varying NOT NULL,
+                "state" text NOT NULL,
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "expires_at" TIMESTAMP,
+                "completed_at" TIMESTAMP,
+                "tenant_id" character varying,
+                CONSTRAINT "PK_StateEntity_id" PRIMARY KEY ("id")
+            )
+        `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "StateEntity" DROP CONSTRAINT "PK_StateStore_id"`)
     await queryRunner.query(`DROP TABLE "StateEntity"`)
   }
 }
--- packages/data-store/src/migrations/sqlite/1708096002272-CreateXStateStore.ts
+++ packages/data-store/src/migrations/sqlite/1708096002272-CreateXStateStore.ts
@@ -3,14 +3,24 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class CreateXStateStore1708096002272 implements MigrationInterface {
   name = 'CreateXStateStore1708096002272'
 
-  public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      `CREATE TABLE "StateEntity" ("id" varchar PRIMARY KEY, "state" varchar(255) NOT NULL, "type" varchar(255) NOT NULL, "created_at" DATETIME NOT NULL DEFAULT (datetime('now')), "updated_at" DATETIME NOT NULL DEFAULT (datetime('now')), "completed_at" DATETIME, "tenant_id" varchar(255) NULL, "ttl" INTEGER NULL)`
-    )
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "StateEntity" (
+                "id" varchar PRIMARY KEY NOT NULL,
+                "step" varchar NOT NULL,
+                "type" varchar NOT NULL,
+                "event_name" varchar NOT NULL,
+                "state" text NOT NULL,
+                "created_at" datetime NOT NULL DEFAULT (datetime('now')),
+                "updated_at" datetime NOT NULL DEFAULT (datetime('now')),
+                "expires_at" datetime,
+                "completed_at" datetime,
+                "tenant_id" varchar
+            )
+        `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "StateEntity"`)
     await queryRunner.query(`DROP TABLE "StateEntity"`)
   }
 }
