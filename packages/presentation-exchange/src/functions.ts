0c3ecb90d0c3bb4afc88d83957caaaae2a962e07
A.G.J. Cate <brummos@gmail.com>
2024-06-13 10:37:29 +0200

    chore: WIP

--- packages/presentation-exchange/src/functions.ts
+++ packages/presentation-exchange/src/functions.ts
@@ -2,7 +2,13 @@ import { dereferenceDidKeysWithJwkSupport, getAgentResolver, getIdentifier, getK
 import { IPEXPresentationSignCallback, IRequiredContext } from './types/IPresentationExchange'
 import { IPresentationDefinition } from '@sphereon/pex'
 import { PresentationPayload, ProofFormat } from '@veramo/core'
-import { CredentialMapper, Optional, OriginalVerifiablePresentation, W3CVerifiablePresentation } from '@sphereon/ssi-types'
+import {
+  CredentialMapper,
+  Optional,
+  OriginalVerifiablePresentation,
+  //SdJwtDecodedVerifiableCredential,
+  W3CVerifiablePresentation
+} from '@sphereon/ssi-types'
 import { Format } from '@sphereon/pex-models'
 
 export async function createPEXPresentationSignCallback(
@@ -15,26 +21,44 @@ export async function createPEXPresentationSignCallback(
   },
   context: IRequiredContext,
 ): Promise<IPEXPresentationSignCallback> {
-  function determineProofFormat({
-    format,
-    presentationDefinition,
-  }: {
-    format?: Format | 'jwt' | 'lds' | 'EthereumEip712Signature2021'
-    presentationDefinition: IPresentationDefinition
-  }) {
-    let proofFormat: ProofFormat = 'jwt'
+  function determineProofFormat(args: {format?: Format | 'jwt' | 'lds' | 'EthereumEip712Signature2021', presentationDefinition: IPresentationDefinition }) {
+    const {format, presentationDefinition } = args
+
+    // All format arguments are optional. So if no format has been given we go for JWT
     const formatOptions = format ?? args.format ?? presentationDefinition.format
-    if (formatOptions) {
-      if (typeof formatOptions === 'object') {
-        const formats = Object.keys(formatOptions).map((form) => (form.includes('ldp') ? 'lds' : 'jwt'))
-        if (!formats.includes('jwt')) {
-          proofFormat = 'lds'
-        }
-      } else {
-        proofFormat = formatOptions
-      }
+    if (!formatOptions) {
+      return 'vc+sd-jwt'
     }
-    return proofFormat
+
+    // if formatOptions is a singular string we can return that as the format
+    if (typeof formatOptions === 'string') {
+      return formatOptions
+    }
+
+    // here we transform all format options to either lds or jwt. but we also want to support sd-jwt, so we need to specifically check for this one. which is ['vc+sd-jwt']
+    const formats = new Set(Object.keys(formatOptions).map((form) => (form.includes('ldp')
+        ? 'lds'
+        : form.includes('vc+sd-jwt')
+          ? 'vc+sd-jwt'
+          : 'jwt'
+    )))
+
+    // if we only have 1 format type we can return that
+    if (formats.size === 1) {
+      return formats.values().next().value
+    }
+
+    // if selective disclosure is an option we go for sd-jwt
+    if (formats.has('vc+sd-jwt')) {
+      return 'vc+sd-jwt'
+    } else
+      // if we can go for jwt, we go for jwt
+    if (formats.has('jwt')) {
+      return 'jwt'
+    }
+
+    // else we go for lds
+    return 'lds'
   }
 
   return async ({
@@ -44,96 +68,140 @@ export async function createPEXPresentationSignCallback(
     format,
     challenge,
   }: {
-    presentation: Optional<PresentationPayload, 'holder'>
+    presentation: Optional<PresentationPayload, 'holder'> //| SdJwtDecodedVerifiableCredential
     presentationDefinition: IPresentationDefinition
     format?: Format | ProofFormat
     domain?: string
     challenge?: string
   }): Promise<W3CVerifiablePresentation> => {
+    const proofFormat = determineProofFormat({ format, presentationDefinition })
+
     const idOpts = args.idOpts
     const id = await getIdentifier(idOpts, context)
-    if (typeof idOpts.identifier === 'string') {
-      idOpts.identifier = id
-    }
-    if (!presentation.holder) {
-      presentation.holder = id.did
-    }
     const key = await getKey(id, 'authentication', context, idOpts.kid)
-    const didResolution = await getAgentResolver(context).resolve(idOpts.identifier.did)
-    const vms = await dereferenceDidKeysWithJwkSupport(didResolution.didDocument!, idOpts.verificationMethodSection ?? 'authentication', context)
-    const vm = vms.find((vm) => vm.publicKeyHex === key.publicKeyHex)
-    if (!vm) {
-      throw Error(`Could not resolve DID document or match signing key to did ${idOpts.identifier.did}`)
-    }
 
-    const proofFormat = determineProofFormat({ format, presentationDefinition })
-    let header
-    if (!presentation.holder) {
-      presentation.holder = id.did
-    }
-    if (proofFormat === 'jwt') {
-      header = {
-        kid: vm.id,
+   // if ('compactSdJwtVc' in presentation) {
+      // const sdJwtPresentation = <SdJwtDecodedVerifiableCredential>presentation
+      //
+      // const sdjwt = new SDJwtVcInstance({
+      //   hasher: this.algorithms.hasher,
+      //   saltGenerator: this.algorithms.saltGenerator,
+      //   kbSigner: signer,
+      //   kbSignAlg: key.type,
+      // })
+
+    //  console.log(`is SD-JWT`)
+
+      // if (proofFormat === "vc+sd-jwt") {
+      //   // TODO sd-jwt presentation only supports 1 vc at the moment
+      //   const credential = (presentation as PresentationPayload).verifiableCredential?.[0]
+      //   if (!credential) {
+      //     return Promise.reject(Error('No credential found in payload for sd-jwt presentation'))
+      //   }
+      //
+      //   CredentialMapper.
+      //
+      //     const vp = await context.agent.createSdJwtVcPresentation({
+      //     presentation: credential,
+      //     presentationFrame: presentationDefinition.frame//{ given_name: true }, // TODO
+      //     kb: {
+      //       payload: {
+      //         aud: Array.isArray(presentation.verifier) // TODO this array check does nothing?
+      //           ? presentation.verifier
+      //           : presentation.verifier ?? domain ?? args.domain,
+      //         iat: presentation.nbf,
+      //         nonce: uuidv4()
+      //       }
+      //     }
+      //   })
+
+        // TODO decode vp
+
+       // return CredentialMapper.storedPresentationToOriginalFormat(vp as OriginalVerifiablePresentation)
+      // }
+//    } else {
+      // const idOpts = args.idOpts
+      // const id = await getIdentifier(idOpts, context)
+      if (typeof idOpts.identifier === 'string') {
+        idOpts.identifier = id
       }
-      if (presentation.verifier || !presentation.aud) {
-        presentation.aud = Array.isArray(presentation.verifier) ? presentation.verifier : presentation.verifier ?? domain ?? args.domain
-        delete presentation.verifier
+      if (!presentation.holder) {
+        presentation.holder = id.did
       }
-      if (!presentation.nbf) {
-        if (presentation.issuanceDate) {
-          const converted = Date.parse(presentation.issuanceDate)
-          if (!isNaN(converted)) {
-            presentation.nbf = Math.floor(converted / 1000)
+      // const key = await getKey(id, 'authentication', context, idOpts.kid)
+      const didResolution = await getAgentResolver(context).resolve(idOpts.identifier.did)
+      const vms = await dereferenceDidKeysWithJwkSupport(didResolution.didDocument!, idOpts.verificationMethodSection ?? 'authentication', context)
+      const vm = vms.find((vm) => vm.publicKeyHex === key.publicKeyHex)
+      if (!vm) {
+        throw Error(`Could not resolve DID document or match signing key to did ${idOpts.identifier.did}`)
+      }
+
+      let header
+      if (proofFormat === 'jwt') {
+        header = {
+          kid: vm.id,
+        }
+        if (presentation.verifier || !presentation.aud) {
+          // TODO check external implementations if they like a single audience instead of an array
+          presentation.aud = (Array.isArray(presentation.verifier) && presentation.verifier.length === 1) ? presentation.verifier[0] : presentation.verifier ?? domain ?? args.domain
+          delete presentation.verifier
+        }
+        if (!presentation.nbf) {
+          if (presentation.issuanceDate) {
+            const converted = Date.parse(presentation.issuanceDate)
+            if (!isNaN(converted)) {
+              presentation.nbf = Math.floor(converted / 1000)
+            }
+          } else {
+            presentation.nbf = Math.floor(Date.now() / 1000)
           }
-        } else {
-          presentation.nbf = Math.floor(Date.now() / 1000)
+        }
+
+        if (!presentation.iat) {
+          presentation.iat = presentation.nbf
+        }
+
+        if (!presentation.exp) {
+          if (presentation.expirationDate) {
+            const converted = Date.parse(presentation.expirationDate)
+            if (!isNaN(converted)) {
+              presentation.exp = Math.floor(converted / 1000)
+            }
+          } else {
+            presentation.exp = presentation.nbf + 600
+          }
+        }
+
+        if (!presentation.vp) {
+          presentation.vp = {}
+        }
+        if (!presentation.sub) {
+          presentation.sub = id.did
+        }
+        if (!presentation.vp.holder) {
+          presentation.vp.holder = id.did
         }
       }
 
-      if (!presentation.iat) {
-        presentation.iat = presentation.nbf
-      }
+      // we ignore the alg / proof_format for now, as we already have the kid anyway at this point
 
-      if (!presentation.exp) {
-        if (presentation.expirationDate) {
-          const converted = Date.parse(presentation.expirationDate)
-          if (!isNaN(converted)) {
-            presentation.exp = Math.floor(converted / 1000)
-          }
-        } else {
-          presentation.exp = presentation.nbf + 600
-        }
-      }
+      // todo: look for jwt_vc_json and remove types and @context
 
-      if (!presentation.vp) {
-        presentation.vp = {}
-      }
-      if (!presentation.sub) {
-        presentation.sub = id.did
-      }
-      if (!presentation.vp.holder) {
-        presentation.vp.holder = id.did
-      }
-    }
-
-    // we ignore the alg / proof_format for now, as we already have the kid anyway at this point
-
-    // todo: look for jwt_vc_json and remove types and @context
-
-    console.log(`PRE CREATE VP AGENT ${new Date().toString()}`)
-    const vp = await context.agent.createVerifiablePresentation({
-      presentation: presentation as PresentationPayload,
-      removeOriginalFields: false,
-      keyRef: key.kid,
-      // domain: domain ?? args.domain, // handled above, and did-jwt-vc creates an array even for 1 entry
-      challenge: challenge ?? args.challenge,
-      fetchRemoteContexts: args.fetchRemoteContexts !== false,
-      proofFormat,
-      header,
-    })
-    console.log(`POST CREATE VP AGENT ${new Date().toString()}`)
-    console.log(`PRE MAPPER AGENT ${new Date().toString()}`)
-    // makes sure we extract an actual JWT from the internal representation in case it is a JWT
-    return CredentialMapper.storedPresentationToOriginalFormat(vp as OriginalVerifiablePresentation)
+      console.log(`PRE CREATE VP AGENT ${new Date().toString()}`)
+      const vp = await context.agent.createVerifiablePresentation({
+        presentation: presentation as PresentationPayload,
+        removeOriginalFields: false,
+        keyRef: key.kid,
+        // domain: domain ?? args.domain, // handled above, and did-jwt-vc creates an array even for 1 entry
+        challenge: challenge ?? args.challenge,
+        fetchRemoteContexts: args.fetchRemoteContexts !== false,
+        proofFormat: proofFormat,
+        header,
+      })
+      console.log(`POST CREATE VP AGENT ${new Date().toString()}`)
+      console.log(`PRE MAPPER AGENT ${new Date().toString()}`)
+      // makes sure we extract an actual JWT from the internal representation in case it is a JWT
+      return CredentialMapper.storedPresentationToOriginalFormat(vp as OriginalVerifiablePresentation)
+   // }
   }
 }
