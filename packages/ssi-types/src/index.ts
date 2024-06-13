eae66f2e93a4fb54520284ed948feca09d829398
Niels Klomp <nklomp@sphereon.com>
2024-06-13 20:55:58 +0200

    fix: Ensure logger is initialized early preventing potential issues when importing from other libraries

--- packages/ssi-types/src/index.ts
+++ packages/ssi-types/src/index.ts
@@ -1,3 +1,7 @@
+import { Loggers } from './logging'
+// We call the logger first, making sure that any library (re-)using the logger has the static initialized
+Loggers.DEFAULT.get('sphereon:ssi').debug(`Sphereon logger initialized`)
+
 export * from './logging'
 export * from './events'
 export * from './types'
