be7dc15537ec005fb7b3745c70dd0b7c4fd75300
Niels Klomp <nklomp@sphereon.com>
2024-07-16 18:41:41 +0200

    fix: Ensure we always use the ES256 key for EBSI auth

--- packages/ebsi-support/src/functions/Attestation.ts
+++ packages/ebsi-support/src/functions/Attestation.ts
@@ -80,8 +80,9 @@ export const ebsiCreateAttestationAuthRequestURL = async (
   }
   // This only works if the DID is actually registered, otherwise use our internal KMS;
   // that is why the offline argument is passed in when type is Verifiable Auth to Onboard, as no DID is present at that point yet
+  // We are getting the ES256 key here, as that is the one needed for auth in EBSI
   const authKey = await getAuthenticationKey(
-    { identifier, offlineWhenNoDIDRegistered: credentialType === 'VerifiableAuthorisationToOnboard', noVerificationMethodFallback: true },
+    { identifier, offlineWhenNoDIDRegistered: credentialType === 'VerifiableAuthorisationToOnboard', noVerificationMethodFallback: true, keyType: 'Secp256r1' },
     context,
   )
   const kid = authKey.meta?.jwkThumbprint ?? calculateJwkThumbprintForKey({ key: authKey })
