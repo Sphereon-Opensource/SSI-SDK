cc19227cba2495264176a1955c14e16402dc8b31
Niels Klomp <nklomp@sphereon.com>
2024-08-25 12:30:58 +0200

    fix: Allow for 1 sec skew in test for VCs

--- packages/ssi-types/__tests__/uniform-claims.test.ts
+++ packages/ssi-types/__tests__/uniform-claims.test.ts
@@ -82,7 +82,7 @@ describe('Uniform VC claims', () => {
     const nbf = new Date().valueOf()
     jwtVc['nbf' as keyof IVerifiableCredential] = nbf / 1000
     ;(<ICredential>jwtVc['vc' as keyof IVerifiableCredential]).issuanceDate = new Date(+new Date() + 2).toISOString()
-    expect(() => CredentialMapper.toUniformCredential(jwtVc, { maxTimeSkewInMS: 10 })).toThrowError(
+    expect(() => CredentialMapper.toUniformCredential(jwtVc, { maxTimeSkewInMS: 1000 })).toThrowError(
       `Inconsistent issuance dates between JWT claim (${new Date(nbf).toISOString().replace(/\.\d\d\dZ/, 'Z')}) and VC value (${
         (<ICredential>jwtVc['vc' as keyof IVerifiableCredential]).issuanceDate
       })`,
