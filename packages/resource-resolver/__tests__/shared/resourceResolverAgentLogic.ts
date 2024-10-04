9523fe7ff024b7e38ee9d2db1508859ced392a23
A.G.J. Cate <brummos@gmail.com>
2024-10-04 17:26:59 +0200

    chore: WIP

--- packages/resource-resolver/__tests__/shared/resourceResolverAgentLogic.ts
+++ packages/resource-resolver/__tests__/shared/resourceResolverAgentLogic.ts
@@ -1,5 +1,5 @@
 import { TAgent } from '@veramo/core'
-import { Request, Response } from 'cross-fetch'
+import { Request } from 'cross-fetch'
 // @ts-ignore
 import nock from 'nock'
 import { IResourceResolver } from '../../src'
