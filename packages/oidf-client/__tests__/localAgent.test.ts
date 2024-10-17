b0a6a2fa3c2fa3f07af201b95a2a1bb302f0d06e
Zoë Maas <mmaas@sphereon.com>
2024-10-17 17:10:34 +0200

    test: Added verify() and registerCryptoServiceCallback() tests

---
+++ packages/oidf-client/__tests__/localAgent.test.ts
@@ -0,0 +1,30 @@
+import { createObjects, getConfig } from '../../agent-config/dist'
+
+jest.setTimeout(60000)
+
+import oidfClientAgentLogic from './shared/oidfClientAgentLogic'
+
+let agent: any
+
+const setup = async (): Promise<boolean> => {
+  const config = await getConfig('packages/oidf-client/agent.yml')
+  const { localAgent } = await createObjects(config, { localAgent: '/agent' })
+  agent = localAgent
+
+  return true
+}
+
+const tearDown = async (): Promise<boolean> => {
+  return true
+}
+
+const getAgent = () => agent
+const testContext = {
+  getAgent,
+  setup,
+  tearDown,
+}
+
+describe('Local integration tests', (): void => {
+  oidfClientAgentLogic(testContext)
+})
