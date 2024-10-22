6fe01f48d1cb38871a019feab126755a125a6d06
Zoë Maas <mmaas@sphereon.com>
2024-10-22 16:14:23 +0200

    fix: Implemented temporary cryptoServiceCallback verify function

--- packages/oidf-client/src/agent/OIDFClient.ts
+++ packages/oidf-client/src/agent/OIDFClient.ts
@@ -12,11 +12,13 @@ import {
     ResolveTrustChainArgs,
     ResolveTrustChainCallbackResult
 } from "../types/IOIDFClient";
+import * as jose from 'jose'
 import {
     com
 } from "../../../../../OpenID-Federation/build/js/packages/openid-federation-modules-openid-federation-client";
 import {schema} from "../index";
 import FederationClient = com.sphereon.oid.fed.client.FederationClient;
+import {JWK, JWTVerifyOptions} from "jose";
 
 export const oidfClientMethods: Array<string> = [
     'resolveTrustChain',
@@ -33,13 +35,39 @@ export class OIDFClient implements IAgentPlugin {
         if (cryptoServiceCallback) {
             this.oidfClient = new FederationClient(null, cryptoServiceCallback)
         } else {
-            // FIXME pass in the actual verification function
+            // FIXME pass in the verification function of the JWSService,
             this.oidfClient = new FederationClient(
                 null, {
-                    q3t: (jwt: string, key: any): Promise<boolean> => {
-                        console.log(`${jwt}:${key} -> Custom callback function reached`)
-                        return Promise.resolve(true)
-                }
+                    q3t: async (jwt: string, key: any): Promise<boolean> => {
+                        // FIXME For some reason the keys is the key object are messed up
+                        const jwk: JWK = {
+                            kty: key.e3s_1,
+                            kid: key.f3s_1,
+                            crv: key.g3s_1,
+                            x: key.h3s_1,
+                            y: key.i3s_1,
+                            n: key.j3s_1,
+                            e: key.k3s_1,
+                            alg: key.l3s_1,
+                            use: key.m3s_1,
+                            x5u: key.n3s_1,
+                            x5c: key.o3s_1,
+                            x5t: key.p3s_1,
+                            'x5t#S256': key.q3s_1,
+                        }
+
+                        const publicKey = await jose.importJWK(jwk)
+
+                        const now = new Date()
+                        const past = now.setDate(now.getDate() - 60)
+
+                        const options: JWTVerifyOptions = {
+                            currentDate: new Date(past)
+                        }
+
+                        const result = await jose.jwtVerify(jwt, publicKey, options)
+                        return result !== undefined
+                    }
             })
         }
     }
