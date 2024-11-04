3614645d3a16056bc30fc44cc112e03e91585078
sanderPostma <spostma@sphereon.com>
2024-11-04 16:13:41 +0100

    chore: fixed nockSetup for unit tests

--- packages/oidf-client/__tests__/localAgent.test.ts
+++ packages/oidf-client/__tests__/localAgent.test.ts
@@ -1,5 +1,5 @@
 import oidfClientAgentLogic from './shared/oidfClientAgentLogic'
-import {createObjects, getConfig} from '../../agent-config/dist'
+import { getConfig, createObjects } from '@sphereon/ssi-sdk.agent-config'
 
 jest.setTimeout(60000)
 
