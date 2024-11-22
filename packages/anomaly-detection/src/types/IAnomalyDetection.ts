4d866d873a546732bd37f6788e42d2fde6d8a911
Zoë Maas <mmaas@sphereon.com>
2024-11-22 14:40:03 +0100

    chore: Added property based Partial 'PartialBy'

--- packages/anomaly-detection/src/types/IAnomalyDetection.ts
+++ packages/anomaly-detection/src/types/IAnomalyDetection.ts
@@ -5,7 +5,9 @@ export interface IAnomalyDetection extends IPluginMethodMap {
   anomalyDetectionLookupLocation(args: AnomalyDetectionLookupLocationArgs, context: IRequiredContext): Promise<AnomalyDetectionLookupLocationResult>
 }
 
-export type AnomalyDetectionLookupLocationArgs = Partial<AnomalyDetectionStoreArgs>
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export type AnomalyDetectionLookupLocationArgs = PartialBy<AnomalyDetectionStoreArgs, 'storeId' | 'namespace'>
 
 export type AnomalyDetectionLookupLocationResult = {
   continent?: string
