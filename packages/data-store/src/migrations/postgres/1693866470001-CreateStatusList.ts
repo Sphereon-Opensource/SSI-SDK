6d68df1930a936857ab86b3292a2b419c51fd28b
sanderPostma <spostma@sphereon.com>
2025-01-17 11:50:50 +0100

    chore: split out migrations to -UpdateStatusList.ts

--- packages/data-store/src/migrations/postgres/1693866470001-CreateStatusList.ts
+++ packages/data-store/src/migrations/postgres/1693866470001-CreateStatusList.ts
@@ -10,39 +10,38 @@ export class CreateStatusList1693866470001 implements MigrationInterface {
     await queryRunner.query(`CREATE TYPE "StatusList_credentialidmode_enum" AS ENUM('ISSUANCE', 'PERSISTENCE', 'NEVER')`)
 
     await queryRunner.query(
+      `CREATE TABLE "StatusListEntry"
+             (
+                 "statusListId"    character varying NOT NULL,
+                 "statusListIndex" integer           NOT NULL,
+                 "credentialId"    character varying,
+                 "credentialHash"  character varying(128),
+                 "correlationId"   character varying(255),
+                 "value"           character varying(50),
+                 CONSTRAINT "PK_68704d2d13857360c6b44a3d1d0" PRIMARY KEY ("statusListId", "statusListIndex")
+             )`,
+    )
+    await queryRunner.query(`CREATE TYPE "StatusList_type_enum" AS ENUM('StatusList2021')`)
+    await queryRunner.query(`CREATE TYPE "StatusList_drivertype_enum" AS ENUM('agent_typeorm', 'agent_kv_store', 'github', 'agent_filesystem')`)
+    await queryRunner.query(`CREATE TYPE "StatusList_credentialidmode_enum" AS ENUM('ISSUANCE', 'PERSISTENCE', 'NEVER')`)
+    await queryRunner.query(
       `CREATE TABLE "StatusList"
              (
-                 "id"                   varchar                            NOT NULL,
-                 "correlationId"        varchar                            NOT NULL,
+                 "id"                   character varying                  NOT NULL,
+                 "correlationId"        character varying                  NOT NULL,
                  "length"               integer                            NOT NULL,
                  "issuer"               text                               NOT NULL,
                  "type"                 "StatusList_type_enum"             NOT NULL DEFAULT 'StatusList2021',
                  "driverType"           "StatusList_drivertype_enum"       NOT NULL DEFAULT 'agent_typeorm',
                  "credentialIdMode"     "StatusList_credentialidmode_enum" NOT NULL DEFAULT 'ISSUANCE',
-                 "proofFormat"          varchar                            NOT NULL DEFAULT 'lds',
+                 "proofFormat"          character varying                  NOT NULL DEFAULT 'lds',
+                 "indexingDirection"    character varying                  NOT NULL DEFAULT 'rightToLeft',
+                 "statusPurpose"        character varying                  NOT NULL DEFAULT 'revocation',
                  "statusListCredential" text,
-                 "indexingDirection"    varchar,
-                 "statusPurpose"        varchar,
-                 "bitsPerStatus"        integer,
-                 "expiresAt"            varchar,
                  CONSTRAINT "UQ_correlationId" UNIQUE ("correlationId"),
                  CONSTRAINT "PK_StatusList_Id" PRIMARY KEY ("id")
              )`,
     )
-
-    await queryRunner.query(
-      `CREATE TABLE "StatusListEntry"
-             (
-                 "statusListId"    varchar NOT NULL,
-                 "statusListIndex" integer NOT NULL,
-                 "credentialId"    varchar,
-                 "credentialHash"  varchar(128),
-                 "correlationId"   varchar(255),
-                 "value"           varchar(50),
-                 CONSTRAINT "PK_68704d2d13857360c6b44a3d1d0" PRIMARY KEY ("statusListId", "statusListIndex")
-             )`,
-    )
-
     await queryRunner.query(
       `ALTER TABLE "StatusListEntry"
                 ADD CONSTRAINT "FK_statusListEntry_statusListId" FOREIGN KEY ("statusListId") REFERENCES "StatusList" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
