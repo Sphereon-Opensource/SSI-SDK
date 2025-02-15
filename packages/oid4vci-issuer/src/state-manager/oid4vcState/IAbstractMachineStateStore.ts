56ec3e0d65066a2331b5f46aab74308405e4bb2a
Niels Klomp <nklomp@sphereon.com>
2025-02-15 16:30:52 +0100

    feat: Add oid4vci state store

---
+++ packages/oid4vci-issuer/src/state-manager/oid4vcState/IAbstractMachineStateStore.ts
@@ -0,0 +1,57 @@
+import { StateType } from '@sphereon/oid4vci-common'
+import { Optional } from '@sphereon/ssi-types'
+import { StoreMachineStateGetArgs, StoreMachineStateInfo } from '../../../../data-store/src'
+import { Oid4vcStateEntity } from '../../../../data-store/src/entities/oid4vcState/Oid4vcStateEntity'
+
+
+export interface IOid4vcStateStore<StateType> {
+  persistOid4vcState(args: Oid4vcStateStoreParams<StateType>): Promise<Oid4vcStatePersisted<StateType>>
+
+  findOid4vcStates(args: StoreOid4vcFindActiveArgs): Promise<Array<Oid4vcStatePersisted<StateType>>>
+
+  getOid4vcState(args: StoreOid4vcGetArgs): Promise<Oid4vcStatePersisted<StateType>>
+
+  deleteOid4vcState(args: StoreOid4vcDeleteArgs): Promise<boolean>
+
+  deleteExpiredOid4vcStates(args: StoreOid4vcDeleteExpiredArgs): Promise<number>
+}
+
+
+export type StoreMachineStatePersistArgs<StateType> = Omit<Oid4vcStateStoreParams<StateType>, 'createdAt' | 'updatedAt'>
+
+export type StoreOid4vcFindActiveArgs = Partial<Pick<Oid4vcStateStoreParams<StateType>, 'expiresAt' | 'tenantId' | 'stateId'>>
+
+export type FindMachineStatesFilterArgs = Array<Partial<Omit<Oid4vcStateStoreParams<StateType>, 'state'>>>
+
+export type StoreFindMachineStatesArgs = {
+  filter: FindMachineStatesFilterArgs
+}
+
+export type StoreOid4vcGetArgs = Pick<Oid4vcStateEntity<StateType>, 'id' | 'stateId' | 'correlationId' | 'lookups' | 'tenantId'>
+export type Oid4vcStateStore<StateType> = Pick<Oid4vcStateEntity<StateType>, 'id' | 'stateId' | 'correlationId' | 'lookups'>
+
+
+export type StoreOid4vcDeleteArgs = StoreOid4vcGetArgs
+export type StoreOid4vcDeleteExpiredArgs = {
+  id?: string;
+  correlationId?: string,
+  sessionId?: string;
+  lookups?: Array<string>;
+  tenantId?: string;
+}
+
+
+export type Oid4vcStatePersisted<StateType> = {
+  id: string
+  stateId?: string
+  correlationId?: string
+  type: string
+  state: StateType
+  lookups?: Array<string>
+  createdAt: Date
+  lastUpdatedAt: Date
+  expiresAt?: Date
+  tenantId?: string
+}
+
+export type Oid4vcStateStoreParams<StateType> = Omit<Oid4vcStatePersisted<StateType>, 'id'>
