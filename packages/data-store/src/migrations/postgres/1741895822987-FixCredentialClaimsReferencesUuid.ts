2d51dd82b660a30ab830416cadc0f2f03a88f5ee
Niels Klomp <nklomp@sphereon.com>
2025-03-13 21:06:34 +0100

    fix: Issue with credential local branding in Postgresql where a uuid column was compared with a varchar

---
+++ packages/data-store/src/migrations/postgres/1741895822987-FixCredentialClaimsReferencesUuid.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class FixCredentialClaimsReferencesUuid1741895822987 implements MigrationInterface {
+  name = 'FixCredentialClaimsReferencesUuid1741895822987'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Migrate varchar to uuid
+    await queryRunner.query(`ALTER TABLE "CredentialClaims"`)
+    await queryRunner.query(`ALTER COLUMN "credentialLocaleBrandingId" TYPE uuid USING "credentialLocaleBrandingId"::uuid;`)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Migrate uuid back to varchar
+    await queryRunner.query(`ALTER TABLE "CredentialClaims"`)
+    await queryRunner.query(`ALTER COLUMN "credentialLocaleBrandingId" TYPE character varying USING "credentialLocaleBrandingId"::text;
+`)
+  }
+}
