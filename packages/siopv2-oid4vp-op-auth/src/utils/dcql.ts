c1721a5d7f5727216bacceaddada3463bbcb9a81
A.G.J. Cate <brummos@gmail.com>
2025-08-26 09:24:22 +0200

    feat: updated siopv2-oid4vp-op-auth plugin to use OID4VP v1

--- packages/siopv2-oid4vp-op-auth/src/utils/dcql.ts
+++ packages/siopv2-oid4vp-op-auth/src/utils/dcql.ts
@@ -1,36 +1,33 @@
 import { UniqueDigitalCredential } from '@sphereon/ssi-sdk.credential-store'
-import { DcqlCredential, DcqlSdJwtVcCredential, DcqlW3cVcCredential } from 'dcql'
 import { CredentialMapper, HasherSync, OriginalVerifiableCredential } from '@sphereon/ssi-types'
+import { Dcql } from '@sphereon/did-auth-siop'
+import { DcqlCredential } from 'dcql'
 import { isUniqueDigitalCredential } from './CredentialUtils'
 
 export function convertToDcqlCredentials(credential: UniqueDigitalCredential | OriginalVerifiableCredential, hasher?: HasherSync): DcqlCredential {
-  let payload
+  let originalVerifiableCredential
   if (isUniqueDigitalCredential(credential)) {
     if (!credential.originalVerifiableCredential) {
       throw new Error('originalVerifiableCredential is not defined in UniqueDigitalCredential')
     }
-    payload = CredentialMapper.decodeVerifiableCredential(credential.originalVerifiableCredential, hasher)
+    originalVerifiableCredential = CredentialMapper.decodeVerifiableCredential(credential.originalVerifiableCredential, hasher)
   } else {
-    payload = CredentialMapper.decodeVerifiableCredential(credential as OriginalVerifiableCredential, hasher)
+    originalVerifiableCredential = CredentialMapper.decodeVerifiableCredential(credential as OriginalVerifiableCredential, hasher)
   }
 
-  if (!payload) {
+  if (!originalVerifiableCredential) {
     throw new Error('No payload found')
   }
 
-  if ('decodedPayload' in payload && payload.decodedPayload) {
-    payload = payload.decodedPayload
+  if (CredentialMapper.isJwtDecodedCredential(originalVerifiableCredential)) {
+    return Dcql.toDcqlJwtCredential(originalVerifiableCredential)
+  } else if (CredentialMapper.isSdJwtDecodedCredential(originalVerifiableCredential)) {
+    return Dcql.toDcqlSdJwtCredential(originalVerifiableCredential)
+  } else if (CredentialMapper.isMsoMdocDecodedCredential(originalVerifiableCredential)) {
+    return Dcql.toDcqlMdocCredential(originalVerifiableCredential)
+  } else if (CredentialMapper.isW3cCredential(originalVerifiableCredential)) {
+    return Dcql.toDcqlJsonLdCredential(originalVerifiableCredential)
   }
 
-  if ('vct' in payload!) {
-    return { vct: payload.vct, claims: payload, credential_format: 'vc+sd-jwt' } satisfies DcqlSdJwtVcCredential // TODO dc+sd-jwt support?
-  } else if ('docType' in payload! && 'namespaces' in payload) {
-    // mdoc
-    return { docType: payload.docType, namespaces: payload.namespaces, claims: payload }
-  } else {
-    return {
-      claims: payload,
-      credential_format: 'jwt_vc_json', // TODO jwt_vc_json-ld support
-    } as DcqlW3cVcCredential
-  }
+  throw Error(`Unable to map credential to DCQL credential. Credential: ${JSON.stringify(originalVerifiableCredential)}`)
 }
