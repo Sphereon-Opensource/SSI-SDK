19eae01bde8abe5a43c0e1c5b0104a7e57bca31d
sanderPostma <spostma@sphereon.com>
2025-08-29 17:25:20 +0200

    chore: removed PresentationDefinitionWithLocation etc

--- packages/oid4vci-holder/src/types/FirstPartyMachine.ts
+++ packages/oid4vci-holder/src/types/FirstPartyMachine.ts
@@ -1,7 +1,7 @@
 import { BaseActionObject, Interpreter, ResolveTypegenMeta, ServiceMap, State, StateMachine, StatesConfig, TypegenDisabled } from 'xstate'
 import { OpenID4VCIClientState } from '@sphereon/oid4vci-client'
 import { DidAuthConfig, Party } from '@sphereon/ssi-sdk.data-store'
-import { PresentationDefinitionWithLocation, RPRegistrationMetadataPayload } from '@sphereon/did-auth-siop'
+import { RPRegistrationMetadataPayload } from '@sphereon/did-auth-siop'
 import { UniqueDigitalCredential } from '@sphereon/ssi-sdk.credential-store'
 import { AuthorizationChallengeCodeResponse } from '@sphereon/oid4vci-common'
 import { IIdentifier } from '@veramo/core'
@@ -150,7 +150,6 @@ export type SiopV2AuthorizationRequestData = {
   clientIdScheme?: string
   clientId?: string
   entityId?: string
-  presentationDefinitions?: PresentationDefinitionWithLocation[]
   dcqlQuery: DcqlQuery
 }
 
