5b941683248cb57c802aa2e2b270df5f634d80e8
sanderPostma <spostma@sphereon.com>
2025-09-04 17:52:36 +0200

    chore: fixing PDManager with new DCQL version

--- packages/ssi-types/src/types/dcql.ts
+++ packages/ssi-types/src/types/dcql.ts
@@ -2,6 +2,10 @@ import { DcqlQuery } from 'dcql'
 
 export interface DcqlQueryPayload {
   definitionId: string
-
   dcqlQuery: DcqlQuery
+}
+
+export interface DcqlQueryPayloadREST {
+  definitionId: string
+  dcqlQuery: string // TODO before PR: We must make sure we manually (de)serialize over network requests
 }
