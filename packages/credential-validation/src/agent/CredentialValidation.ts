ea2eb39bde3773fae9b9b13bc780b269baec0fd1
A.G.J. Cate <brummos@gmail.com>
2025-09-10 15:24:11 +0200

    chore: cleanup

--- packages/credential-validation/src/agent/CredentialValidation.ts
+++ packages/credential-validation/src/agent/CredentialValidation.ts
@@ -1,4 +1,5 @@
 import mdocPkg from '@sphereon/kmp-mdoc-core'
+const { com } = mdocPkg
 import { IVerifySdJwtVcResult } from '@sphereon/ssi-sdk.sd-jwt'
 import {
   CredentialMapper,
@@ -176,7 +177,7 @@ export class CredentialValidation implements IAgentPlugin {
   private async cvVerifyMdoc(args: VerifyMdocCredentialArgs, context: RequiredContext): Promise<VerificationResult> {
     const { credential } = args
 
-    const issuerSigned = IssuerSignedCbor.Static.cborDecode(decodeFrom(credential, mdocPkg.com.sphereon.kmp.Encoding.BASE64URL))
+    const issuerSigned = IssuerSignedCbor.Static.cborDecode(decodeFrom(credential, com.sphereon.kmp.Encoding.BASE64URL))
 
     const verification = await context.agent.mdocVerifyIssuerSigned({ input: issuerSigned.toJson().issuerAuth }).catch((error: Error) => {
       console.error(error)
