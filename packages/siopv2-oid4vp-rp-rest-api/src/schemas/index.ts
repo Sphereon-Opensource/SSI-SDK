18c5dba66cb2e0081f75ddda320a873dd9ffed23
sanderPostma <spostma@sphereon.com>
2025-09-23 10:04:03 +0200

    chore: PR feedback

--- packages/siopv2-oid4vp-rp-rest-api/src/schemas/index.ts
+++ packages/siopv2-oid4vp-rp-rest-api/src/schemas/index.ts
@@ -1,23 +1,18 @@
-import {
-  CallbackOptsSchema,
-  RequestUriMethod,
-  ResponseMode,
-  ResponseType
-} from '@sphereon/did-auth-siop'
+import { CallbackOptsSchema, RequestUriMethod, ResponseMode, ResponseType } from '@sphereon/did-auth-siop'
 import { z } from 'zod'
 
-export const ResponseTypeSchema = z.enum([ResponseType.VP_TOKEN]);
+export const ResponseTypeSchema = z.enum([ResponseType.VP_TOKEN])
 
-export const ResponseModeSchema = z.enum([ResponseMode.DIRECT_POST, ResponseMode.DIRECT_POST_JWT]);
+export const ResponseModeSchema = z.enum([ResponseMode.DIRECT_POST, ResponseMode.DIRECT_POST_JWT])
 
-const requestUriMethods = ['get', 'post'] as const satisfies Array<RequestUriMethod>;
-export const RequestUriMethodSchema = z.enum(requestUriMethods);
+const requestUriMethods = [RequestUriMethod.GET, RequestUriMethod.POST] as const satisfies Array<RequestUriMethod>
+export const RequestUriMethodSchema = z.enum(requestUriMethods)
 
 export const QRCodeOptsSchema = z.object({
   size: z.number().optional(),
   color_dark: z.string().optional(),
   color_light: z.string().optional(),
-});
+})
 
 export const CreateAuthorizationRequestBodySchema = z.object({
   query_id: z.string(),
@@ -31,7 +26,7 @@ export const CreateAuthorizationRequestBodySchema = z.object({
   qr_code: QRCodeOptsSchema.optional(),
   direct_post_response_redirect_uri: z.string().optional(),
   callback: CallbackOptsSchema.optional(),
-});
+})
 
 export const CreateAuthorizationResponseSchema = z.object({
   correlation_id: z.string(),
@@ -39,4 +34,4 @@ export const CreateAuthorizationResponseSchema = z.object({
   request_uri: z.string(),
   status_uri: z.string(),
   qr_uri: z.string().optional(),
-});
+})
