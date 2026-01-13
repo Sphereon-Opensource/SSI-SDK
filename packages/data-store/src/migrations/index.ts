c2cf3cf022daf6c741c07a398908c51f1e869933
Niels Klomp <nklomp@sphereon.com>
2026-01-13 18:49:26 +0100

    chore: migration fix

--- packages/data-store/src/migrations/index.ts
+++ packages/data-store/src/migrations/index.ts
@@ -1,6 +1,5 @@
 export {
   DataStoreMigrations,
-  DataStoreMigrationsWithVeramo,
   DataStoreEventLoggerMigrations,
   DataStoreContactMigrations,
   DataStoreIssuanceBrandingMigrations,
@@ -9,6 +8,4 @@ export {
   DataStoreMachineStateMigrations,
   DataStorePresentationDefinitionMigrations,
   DataStoreServiceMigrations,
-  VeramoDataStoreMigrations,
-  VeramoDataStoreEntities,
 } from './generic'
