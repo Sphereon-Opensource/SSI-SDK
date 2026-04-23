77f49b99c4b2033c1153d1aa95251319ff1c10ae
sander <spostma@sphereon.com>
2026-04-23 15:55:23 +0200

    fix: request entity too large

--- packages/ssi-express-support/src/express-builders.ts
+++ packages/ssi-express-support/src/express-builders.ts
@@ -41,6 +41,7 @@ export class ExpressBuilder {
   private _server?: http.Server | undefined
   private _terminator?: HttpTerminator
   private _morgan?: Handler<any, any> | undefined
+  private _maxRequestBodySize?: string
 
   private constructor(opts?: { existingExpress?: Express; envVarPrefix?: string }) {
     const { existingExpress, envVarPrefix } = opts ?? {}
@@ -56,6 +57,9 @@ export class ExpressBuilder {
 
   public static fromServerOpts(opts: IExpressServerOpts & { envVarPrefix?: string }) {
     const builder = new ExpressBuilder({ existingExpress: opts?.existingExpress, envVarPrefix: opts?.envVarPrefix })
+    if (opts.maxRequestBodySize) {
+      builder.withMaxRequestBodySize(opts.maxRequestBodySize)
+    }
     return builder.withEnableListenOpts({ ...opts, hostnameOrIP: opts.hostname, startOnBuild: opts.startListening ?? false })
   }
 
@@ -133,6 +137,11 @@ export class ExpressBuilder {
 
   public withEnforcer(enforcer: Enforcer): this {
     this._enforcer = enforcer
+    return this
+  }
+
+  public withMaxRequestBodySize(limit: string): this {
+    this._maxRequestBodySize = limit
     return this
   }
 
@@ -261,8 +270,9 @@ export class ExpressBuilder {
     // @ts-ignore
     opts?.handlers && app.use(opts.handlers)
     //fixme: this should come from the config
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json({ limit: '5mb' }))
+    const limit = this._maxRequestBodySize ?? '5mb'
+    app.use(bodyParser.urlencoded({ extended: true, limit }))
+    app.use(bodyParser.json({ limit }))
     return app
   }
 }
